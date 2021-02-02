6ec67dd6f2a016b7b7315e42fa7e06c83e0c9a09
James Prestwich <prestwich@clabs.co>
2021-02-02 13:18:48 -0800

    feat: add hh task for deploy replica

--- solidity/scripts/deploy.js
+++ solidity/scripts/deploy.js
@@ -1,8 +1,59 @@
-// const { hre } = require('hardhat');
 const { types } = require('hardhat/config');
 
 task('deploy-home')
   .addParam('slip44', 'The origin chain SLIP44 ID', undefined, types.int)
-  .addParam('updater', 'The origin chain updater', undefined, types.string)
-  .addParam('currentRoot', 'The current root')
-  .setAction(async (args) => {});
+  .addParam(
+    'sortition',
+    'The updater identity handler',
+    undefined,
+    types.string,
+  )
+  .setAction(async (args) => {
+    let address = ethers.utils.getAddress(args.sortition);
+
+    let home = await optics.deployHome(args.slip44, address);
+    console.log(home.address);
+  });
+
+task('deploy-replica')
+  .addParam('origin', 'The origin chain SLIP44 ID', undefined, types.int)
+  .addParam(
+    'destination',
+    'The destination chain SLIP44 ID',
+    undefined,
+    types.int,
+  )
+  .addParam('updater', 'The address of the updater', undefined, types.string)
+  .addOptionalParam(
+    'wait',
+    'The optimistic wait period in seconds',
+    60 * 60 * 2, // 2 hours
+    types.int,
+  )
+  .addOptionalParam(
+    'current',
+    'The current root to init with',
+    `0x${'00'.repeat(32)}`,
+    types.string,
+  )
+  .addOptionalParam(
+    'lastProcessed',
+    'The last processed message sequence',
+    0,
+    types.int,
+  )
+  .setAction(async (args) => {
+    let updater = ethers.utils.getAddress(args.updater);
+    if (!ethers.utils.isHexString(args.current, 32)) {
+      throw new Error('current must be a 32-byte 0x prefixed hex string');
+    }
+
+    await optics.deployReplica(
+      args.origin,
+      args.destination,
+      updater,
+      args.wait,
+      args.current,
+      args.lastProcessed,
+    );
+  });
