e30d08fd83bdfe7dc07006b2f3a907fa37e75b06
ltchang <ltchang@stanford.edu>
2021-02-03 12:39:40 -0800

    refactor: changes doubleUpdate function to take single oldRoot rather than array

--- solidity/test/Home.test.js
+++ solidity/test/Home.test.js
@@ -7,100 +7,135 @@ const ACTIVE = 0;
 const FAILED = 1;
 
 describe('Home', async () => {
-    let home;
-    const originSLIP44 = 1234;
-    
-    let [signer, fakeSigner] = provider.getWallets();
-    let updater = new optics.Updater(signer, originSLIP44);
-    const fakeUpdater = new optics.Updater(fakeSigner, originSLIP44);
+  let home;
+  const originSLIP44 = 1234;
 
-    const enqueueMessageAndSuggestUpdate = async (message, recipient) => {
-        message = ethers.utils.formatBytes32String(message);
-        recipient = ethers.utils.formatBytes32String(recipient);
-        await home.enqueue(originSLIP44, recipient, message);
-        return await home.suggestUpdate();
-    }
+  let [signer, fakeSigner] = provider.getWallets();
+  let updater = new optics.Updater(signer, originSLIP44);
+  const fakeUpdater = new optics.Updater(fakeSigner, originSLIP44);
 
-      beforeEach(async () => {      
-        const mockSortition = await deployMockContract(signer, NoSortition.abi);
-        await mockSortition.mock.current.returns(signer.address);
-        await mockSortition.mock.slash.returns();
+  const enqueueMessageAndSuggestUpdate = async (message, recipient) => {
+    message = ethers.utils.formatBytes32String(message);
+    recipient = ethers.utils.formatBytes32String(recipient);
+    await home.enqueue(originSLIP44, recipient, message);
+    return await home.suggestUpdate();
+  };
 
-        const Home = await ethers.getContractFactory('TestHome');
-        home = await Home.deploy(originSLIP44, mockSortition.address);
-        await home.deployed();
-      });
+  beforeEach(async () => {
+    const mockSortition = await deployMockContract(signer, NoSortition.abi);
+    await mockSortition.mock.current.returns(signer.address);
+    await mockSortition.mock.slash.returns();
 
-      it('Halts on fail', async () => {
-        await home.setFailed();
-        expect(await home.state()).to.equal(FAILED);
+    const Home = await ethers.getContractFactory('TestHome');
+    home = await Home.deploy(originSLIP44, mockSortition.address);
+    await home.deployed();
+  });
 
-        const recipient = ethers.utils.formatBytes32String("recipient");
-        const message = ethers.utils.formatBytes32String("message");
-        await expect(home.enqueue(originSLIP44, recipient, message))
-          .to.be.revertedWith('failed state');
-      });
+  it('Halts on fail', async () => {
+    await home.setFailed();
+    expect(await home.state()).to.equal(FAILED);
 
-      it('Suggests current root and latest root on suggestUpdate', async () => {
-        const currentRoot = await home.current();
+    const recipient = ethers.utils.formatBytes32String('recipient');
+    const message = ethers.utils.formatBytes32String('message');
+    await expect(
+      home.enqueue(originSLIP44, recipient, message),
+    ).to.be.revertedWith('failed state');
+  });
 
-        const recipient = ethers.utils.formatBytes32String("recipient");
-        const message = ethers.utils.formatBytes32String("message");
-        await home.enqueue(originSLIP44, recipient, message);
-        const latestEnqueuedRoot = await home.latestEnqueuedRoot();
+  it('Suggests current root and latest root on suggestUpdate', async () => {
+    const currentRoot = await home.current();
 
-        const [suggestedCurrent, suggestedNew] = await home.suggestUpdate();
-        expect(suggestedCurrent).to.equal(currentRoot);
-        expect(suggestedNew).to.equal(latestEnqueuedRoot);
-      });
+    const recipient = ethers.utils.formatBytes32String('recipient');
+    const message = ethers.utils.formatBytes32String('message');
+    await home.enqueue(originSLIP44, recipient, message);
+    const latestEnqueuedRoot = await home.latestEnqueuedRoot();
 
-      it('Accepts a valid update', async () => {
-        const [oldRoot, newRoot] = await enqueueMessageAndSuggestUpdate("message", "recipient");
-        const { signature } = await updater.signUpdate(oldRoot, newRoot);
-        await expect(home.update(oldRoot, newRoot, signature))
-          .to.emit(home, 'Update')
-          .withArgs(originSLIP44, oldRoot, newRoot, signature);
-      });
+    const [suggestedCurrent, suggestedNew] = await home.suggestUpdate();
+    expect(suggestedCurrent).to.equal(currentRoot);
+    expect(suggestedNew).to.equal(latestEnqueuedRoot);
+  });
 
-      it('Rejects update that does not build off of current root', async () => {
-        const [_firstRoot, secondRoot] = await enqueueMessageAndSuggestUpdate("message", "recipient");
-        const [_, thirdRoot] = await enqueueMessageAndSuggestUpdate("message2", "recipient2");
-        
-        // Try to submit update that skips the current (first) root
-        const { signature } = await updater.signUpdate(secondRoot, thirdRoot);
-        await expect(home.update(secondRoot, thirdRoot, signature))
-          .to.be.revertedWith('not a current update');
-      });
+  it('Accepts a valid update', async () => {
+    const [oldRoot, newRoot] = await enqueueMessageAndSuggestUpdate(
+      'message',
+      'recipient',
+    );
+    const { signature } = await updater.signUpdate(oldRoot, newRoot);
+    await expect(home.update(oldRoot, newRoot, signature))
+      .to.emit(home, 'Update')
+      .withArgs(originSLIP44, oldRoot, newRoot, signature);
+  });
 
-      it('Rejects update that does not exist in queue', async () => {
-        const [oldRoot, _newRoot] = await enqueueMessageAndSuggestUpdate("message", "recipient");
-        const fakeNewRoot = ethers.utils.formatBytes32String("fake root"); // better way to create fake root?
-        const { signature } = await updater.signUpdate(oldRoot, fakeNewRoot);
-        await expect(home.update(oldRoot, fakeNewRoot, signature))
-          .to.emit(home, 'ImproperUpdate');
+  it('Rejects update that does not build off of current root', async () => {
+    const [_firstRoot, secondRoot] = await enqueueMessageAndSuggestUpdate(
+      'message',
+      'recipient',
+    );
+    const [_, thirdRoot] = await enqueueMessageAndSuggestUpdate(
+      'message2',
+      'recipient2',
+    );
 
-        expect(await home.state()).to.equal(FAILED);
-      });
+    // Try to submit update that skips the current (first) root
+    const { signature } = await updater.signUpdate(secondRoot, thirdRoot);
+    await expect(
+      home.update(secondRoot, thirdRoot, signature),
+    ).to.be.revertedWith('not a current update');
+  });
 
-      it('Rejects update from non-updater address', async () => {
-        const [oldRoot, newRoot] = await enqueueMessageAndSuggestUpdate("message", "recipient");
-        const { signature: fakeSignature } = await fakeUpdater.signUpdate(oldRoot, newRoot);
-        await expect(home.update(oldRoot, newRoot, fakeSignature))
-          .to.be.revertedWith("bad sig")
-      });
+  it('Rejects update that does not exist in queue', async () => {
+    const [oldRoot, _newRoot] = await enqueueMessageAndSuggestUpdate(
+      'message',
+      'recipient',
+    );
+    const fakeNewRoot = ethers.utils.formatBytes32String('fake root'); // better way to create fake root?
+    const { signature } = await updater.signUpdate(oldRoot, fakeNewRoot);
+    await expect(home.update(oldRoot, fakeNewRoot, signature)).to.emit(
+      home,
+      'ImproperUpdate',
+    );
 
-      it('Fails on valid double update proof', async () => {
-        const [firstRoot, secondRoot] = await enqueueMessageAndSuggestUpdate("message", "recipient");
-        const [_secondRoot, thirdRoot] = await enqueueMessageAndSuggestUpdate("message2", "recipient2");
-        const { signature } = await updater.signUpdate(firstRoot, secondRoot);
-        const { signature: signature2 } = await updater.signUpdate(firstRoot, thirdRoot);
+    expect(await home.state()).to.equal(FAILED);
+  });
 
-        await expect(home.doubleUpdate(
-          [firstRoot, firstRoot], 
-          [secondRoot, thirdRoot], 
-          signature, signature2
-        )).to.emit(home, 'DoubleUpdate');
+  it('Rejects update from non-updater address', async () => {
+    const [oldRoot, newRoot] = await enqueueMessageAndSuggestUpdate(
+      'message',
+      'recipient',
+    );
+    const { signature: fakeSignature } = await fakeUpdater.signUpdate(
+      oldRoot,
+      newRoot,
+    );
+    await expect(
+      home.update(oldRoot, newRoot, fakeSignature),
+    ).to.be.revertedWith('bad sig');
+  });
 
-        expect(await home.state()).to.equal(FAILED);
-      });
+  it('Fails on valid double update proof', async () => {
+    const [firstRoot, secondRoot] = await enqueueMessageAndSuggestUpdate(
+      'message',
+      'recipient',
+    );
+    const [_firstRoot, thirdRoot] = await enqueueMessageAndSuggestUpdate(
+      'message2',
+      'recipient2',
+    );
+    const { signature } = await updater.signUpdate(firstRoot, secondRoot);
+    const { signature: signature2 } = await updater.signUpdate(
+      firstRoot,
+      thirdRoot,
+    );
+
+    await expect(
+      home.doubleUpdate(
+        firstRoot,
+        [secondRoot, thirdRoot],
+        signature,
+        signature2,
+      ),
+    ).to.emit(home, 'DoubleUpdate');
+
+    expect(await home.state()).to.equal(FAILED);
+  });
 });
