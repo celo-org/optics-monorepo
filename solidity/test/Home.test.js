c286b32ad01f80a46007cc2af82528f5ffb7bd2b
ltchang <ltchang@stanford.edu>
2021-02-02 16:47:12 -0800

    feature: adds tests for skipping current root and submitting update with non-existent new root

--- solidity/test/Home.test.js
+++ solidity/test/Home.test.js
@@ -26,13 +26,12 @@ describe('Home', async () => {
 
       it('Halts on fail', async () => {
         await home.setFailed();
-        
-        const state = await home.state();
-        expect(state).to.equal(FAILED);
+        expect(await home.state()).to.equal(FAILED);
 
         const recipient = ethers.utils.formatBytes32String("recipient");
         const message = ethers.utils.formatBytes32String("message");
-        await expect(home.enqueue(originSLIP44, recipient, message)).to.be.revertedWith('Failed state');
+        await expect(home.enqueue(originSLIP44, recipient, message))
+          .to.be.revertedWith('Failed state');
       });
 
       it('Accepts a valid update', async () => {
@@ -47,6 +46,34 @@ describe('Home', async () => {
           .withArgs(originSLIP44, oldRoot, newRoot, signature);
       });
 
-      it('Fails on a valid double update proof', async () => {
+      it('Rejects update that does not build off the current root', async () => {
+        const recipient = ethers.utils.formatBytes32String("recipient");
+        const message = ethers.utils.formatBytes32String("message");
+        const recipient2 = ethers.utils.formatBytes32String("recipient2");
+        const message2 = ethers.utils.formatBytes32String("message2");
+
+        await home.enqueue(originSLIP44, recipient, message);
+        const [_firstRoot, secondRoot] = await home.suggestUpdate();
+        await home.enqueue(originSLIP44, recipient2, message2);
+        const [_secondRoot, thirdRoot] = await home.suggestUpdate();
+        
+        // Try to submit update that skips the current (first) root
+        const { signature } = await updater.signUpdate(secondRoot, thirdRoot);
+        await expect(home.update(secondRoot, thirdRoot, signature))
+          .to.be.revertedWith('Not a current update');
+      });
+
+      it('Rejects update that does not exist in the queue', async () => {
+        const recipient = ethers.utils.formatBytes32String("recipient");
+        const message = ethers.utils.formatBytes32String("message");
+        await home.enqueue(originSLIP44, recipient, message);
+        
+        const [oldRoot, _newRoot] = await home.suggestUpdate();
+        const fakeNewRoot = ethers.utils.formatBytes32String("fake root"); // better way to create fake root?
+        const { signature } = await updater.signUpdate(oldRoot, fakeNewRoot);
+        await expect(home.update(oldRoot, fakeNewRoot, signature))
+          .to.emit(home, 'ImproperUpdate')
+
+        expect(await home.state()).to.equal(FAILED);
       });
 });
