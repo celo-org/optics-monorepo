ac1236982bb248f2dcec1202620fa7ca653aa92f
ltchang <ltchang@stanford.edu>
2021-02-03 12:28:12 -0800

    refactor: split polling updates and polling confirms into joined helper functions

--- rust/relayer/src/relayer.rs
+++ rust/relayer/src/relayer.rs
@@ -1,12 +1,10 @@
 use async_trait::async_trait;
-use color_eyre::{Result};
+use color_eyre::{eyre::ensure, Result};
 use std::sync::Arc;
 use tokio::time::{interval, Interval};
 
 use optics_base::agent::OpticsAgent;
-use optics_core::{
-    traits::{Home, Replica},
-};
+use optics_core::traits::{Home, Replica};
 
 /// A relayer agent
 #[derive(Debug)]
@@ -17,9 +15,38 @@ pub struct Relayer {
 impl Relayer {
     /// Instantiate a new relayer
     pub fn new(interval_seconds: u64) -> Self {
-        Self {
-            interval_seconds,
+        Self { interval_seconds }
+    }
+
+    async fn poll_updates(
+        &self,
+        home: Arc<Box<dyn Home>>,
+        replica: Arc<Box<dyn Replica>>,
+    ) -> Result<()> {
+        // Get replica's current root
+        let old_root = replica.current_root().await?;
+
+        // Check for first signed update building off of the replica's current root
+        let signed_update_opt = home.signed_update_by_old_root(old_root).await?;
+
+        // If signed update exists, update replica's current root
+        if let Some(signed_update) = signed_update_opt {
+            replica.update(&signed_update).await?;
         }
+
+        Ok(())
+    }
+
+    async fn poll_confirms(&self, replica: Arc<Box<dyn Replica>>) -> Result<()> {
+        // Check for pending update that can be confirmed
+        let can_confirm = replica.can_confirm().await?;
+
+        // If valid pending update exists, confirm it
+        if can_confirm {
+            replica.confirm().await?;
+        }
+
+        Ok(())
     }
 
     #[doc(hidden)]
@@ -30,34 +57,18 @@ impl Relayer {
 
 #[async_trait]
 impl OpticsAgent for Relayer {
-    async fn run(
-        &self,
-        home: Arc<Box<dyn Home>>,
-        _replica: Option<Box<dyn Replica>>,
-    ) -> Result<()> {
-        let replica = _replica.expect("relayer must have replica");
-        
+    async fn run(&self, home: Arc<Box<dyn Home>>, replica: Option<Box<dyn Replica>>) -> Result<()> {
+        ensure!(replica.is_some(), "Relayer must have replica.");
+        let replica = Arc::new(replica.unwrap());
         let mut interval = self.interval();
         loop {
-            // Get replica's current root
-            let old_root = replica.current_root().await?;
+            let (updated, confirmed) = tokio::join!(
+                self.poll_updates(home.clone(), replica.clone()),
+                self.poll_confirms(replica.clone())
+            );
 
-            // Check for first signed update building off of the replica's current root
-            let signed_update_opt = home.signed_update_by_old_root(old_root).await?;
-
-            // If signed update exists, update replica's current root
-            if let Some(signed_update) = signed_update_opt {
-                replica.update(&signed_update).await?;
-            }
-
-            // Check for pending update that can be confirmed
-            let can_confirm = replica.can_confirm().await?;
-
-            // If valid pending update exists, confirm it
-            if can_confirm {
-                replica.confirm().await?;
-            }
-
+            updated?;
+            confirmed?;
             interval.tick().await;
         }
     }
