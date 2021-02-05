c91faffc8c782753dac375e896cf86b5e0b1fcc8
Luke Tchang <ltchang@stanford.edu>
2021-02-05 13:45:24 -0800

    fix: fixes message indexing math in Common

--- solidity/contracts/Common.sol
+++ solidity/contracts/Common.sol
@@ -57,15 +57,15 @@ library Message {
     }
 
     function sequence(bytes29 _message) internal pure returns (uint32) {
-        return uint32(_message.indexUint(32, 4));
-    }
-
-    function destination(bytes29 _message) internal pure returns (uint32) {
         return uint32(_message.indexUint(36, 4));
     }
 
+    function destination(bytes29 _message) internal pure returns (uint32) {
+        return uint32(_message.indexUint(40, 4));
+    }
+
     function recipient(bytes29 _message) internal pure returns (bytes32) {
-        return _message.index(40, 32);
+        return _message.index(44, 32);
     }
 
     function recipientAddress(bytes29 _message)
@@ -77,7 +77,7 @@ library Message {
     }
 
     function body(bytes29 _message) internal pure returns (bytes29) {
-        return _message.slice(72, _message.len() - 72, 0);
+        return _message.slice(76, _message.len() - 76, 0);
     }
 }
 
