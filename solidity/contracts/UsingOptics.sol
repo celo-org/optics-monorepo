64f614c563777f80faa4c49b133da7f016e5d7a3
James Prestwich <prestwich@clabs.co>
2021-02-10 16:25:16 -0800

    ref: break out shared tools for devs into UsingOptics.sol

---
+++ solidity/contracts/UsingOptics.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity >=0.6.11;
+
+import "./Home.sol";
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+interface OpticsHandlerI {
+    function handle(
+        uint32 origin,
+        bytes32 sender,
+        bytes memory message
+    ) external returns (bytes memory);
+}
+
+abstract contract UsingOptics is Ownable {
+    mapping(address => uint32) public replicas;
+    Home home;
+
+    constructor() Ownable() {}
+
+    function isReplica(address _replica) internal view returns (bool) {
+        return replicas[_replica] != 0;
+    }
+
+    function enrollReplica(uint32 _domain, address _replica) public onlyOwner {
+        replicas[_replica] = _domain;
+    }
+
+    function unenrollReplica(address _replica) public onlyOwner {
+        replicas[_replica] = 0;
+    }
+
+    function setHome(address _home) public onlyOwner {
+        home = Home(_home);
+    }
+
+    modifier onlyReplica() {
+        require(isReplica(msg.sender), "!replica");
+        _;
+    }
+
+    function coerceBytes32(string memory _s)
+        internal
+        pure
+        returns (bytes32 _b)
+    {
+        assembly {
+            _b := mload(add(_s, 0x20))
+        }
+    }
+}
