e3507178480a9651b082204517aab613b2566b0a
Luke Tchang <ltchang@stanford.edu>
2021-02-16 19:02:51 -0800

    fix: simplifies error handling, adds retry counter for failed api calls, and clones incremental merkle

--- rust/optics-core/src/accumulator/prover_sync.rs
+++ rust/optics-core/src/accumulator/prover_sync.rs
@@ -1,10 +1,7 @@
-use crate::{
-    accumulator::{
+use crate::{accumulator::{
         incremental::IncrementalMerkle,
         prover::{Prover, ProverError},
-    },
-    traits::Home,
-};
+    }, traits::{ChainCommunicationError, Home}};
 use color_eyre::Result;
 use ethers::core::types::H256;
 use std::{sync::Arc, time::Duration};
@@ -17,10 +14,12 @@ use tokio::{
 };
 
 /// Struct to sync prover.
+#[derive(Debug)]
 pub struct ProverSync {
     prover: Arc<RwLock<Prover>>,
     home: Arc<Box<dyn Home>>,
     incremental: IncrementalMerkle,
+    num_retries: i32,
     rx: Receiver<()>,
 }
 
@@ -36,11 +35,11 @@ pub enum ProverSyncError {
         new_root: H256,
     },
     /// ProverSync attempts Prover operation and receives ProverError
-    #[error("Prover error occurred: {error}.")]
-    ProverError {
-        /// Error returned by Prover operation
-        error: ProverError,
-    },
+    #[error(transparent)]
+    ProverError(ProverError),
+    /// ProverSync receives ChainCommunicationError from chain API
+    #[error(transparent)]
+    ChainCommunicationError(ChainCommunicationError),
 }
 
 impl ProverSync {
@@ -48,9 +47,11 @@ impl ProverSync {
     /// local merkle tree with all leaves between local root and
     /// new root. Use short interval for bootup syncing and longer
     /// interval for regular polling.
+    #[tracing::instrument(err)]
     async fn poll_updates(&mut self, interval_seconds: u64) -> Result<(), ProverSyncError> {
         let mut interval = interval(Duration::from_secs(interval_seconds));
 
+        let mut num_retries = self.num_retries.clone();
         loop {
             let local_root = self.prover.read().await.root();
 
@@ -58,7 +59,12 @@ impl ProverSync {
 
             // If error polling signed update, log error and try again
             if let Err(e) = signed_update_res {
+                if num_retries <= 0 {
+                    return Err(ProverSyncError::ChainCommunicationError(e));
+                }
+                
                 tracing::error!("Error retrieving signed_update: {}", e);
+                num_retries -= 1;
                 continue;
             }
 
@@ -80,57 +86,66 @@ impl ProverSync {
     /// produced between `local_root` and `new_root`. If successful (i.e.
     /// incremental tree is updated until its root equals the `new_root`),
     /// commit to changes by batch updating the prover's actual merkle tree.
+    #[tracing::instrument(err)]
     async fn update_prover_tree(
         &mut self,
         local_root: H256,
         new_root: H256,
     ) -> Result<(), ProverSyncError> {
-        let mut leaves: Vec<H256> = Vec::new();
-
         // If roots don't match by end of incremental update, will return
         // MismatchedRoots error
-        self.update_incremental(local_root, new_root, &mut leaves)
-            .await?;
+        let leaves = self.update_incremental_and_return_leaves(local_root, new_root).await?;
 
         let mut prover_write = self.prover.write().await;
-        for leaf in leaves.into_iter() {
-            if let Err(e) = prover_write.ingest(leaf) {
-                return Err(ProverSyncError::ProverError { error: e });
-            }
-        }
-
+        prover_write.extend(leaves.into_iter());
+        assert_eq!(new_root, prover_write.root());
+        
         Ok(())
     }
 
     /// Given `local_root` and `new_root` from signed update, ingest leaves
-    /// into incremental merkle one-by-one until local root matches new root.
-    /// If incremental merkle is up-to-date with update but roots still don't
-    /// match, return `MismatchedRoots` error.
-    async fn update_incremental(
+    /// into incremental merkle one-by-one until local root matches new root
+    /// and return ingested leaves if successful. If incremental merkle is 
+    /// up-to-date with update but roots still don't match, return 
+    /// `MismatchedRoots` error.
+    #[tracing::instrument(err)]
+    async fn update_incremental_and_return_leaves(
         &mut self,
-        mut local_root: H256,
+        local_root: H256,
         new_root: H256,
-        leaves: &mut Vec<H256>,
-    ) -> Result<(), ProverSyncError> {
+    ) -> Result<Vec<H256>, ProverSyncError> {
+        let mut leaves: Vec<H256> = Vec::new();
+        
+        // Create clone of ProverSync's incremental so we can easily discard
+        // changes in case of bad update
+        let mut incremental = self.incremental.clone();
+        let mut local_root = local_root;
+
+        let mut num_retries = self.num_retries.clone();
         while local_root != new_root {
-            let tree_size = self.incremental.count();
-            let leaf_res = self.home.leaf_by_tree_size(tree_size).await;
+            let tree_size = incremental.count();
+            let leaf_res = self.home.leaf_by_tree_index(tree_size).await;
 
             // If error retrieving leaf, log error and try again
             if let Err(e) = leaf_res {
+                if num_retries <= 0 {
+                    return Err(ProverSyncError::ChainCommunicationError(e));
+                }
+
                 tracing::error!("Error retrieving leaf at tree_size {}: {}", tree_size, e);
+                num_retries -= 1;
                 continue;
             }
 
             // If new leaf exists, try to ingest into incremental
             if let Some(leaf) = leaf_res.unwrap() {
-                self.incremental.ingest(leaf);
+                incremental.ingest(leaf);
                 leaves.push(leaf);
-                local_root = self.incremental.root();
+                local_root = incremental.root();
             } else {
                 // If local incremental tree up-to-date but doesn't match new
                 // root, bubble up MismatchedRoots error
-                local_root = self.incremental.root();
+                local_root = incremental.root();
                 if local_root != new_root {
                     return Err(ProverSyncError::MismatchedRoots {
                         local_root,
@@ -140,6 +155,7 @@ impl ProverSync {
             }
         }
 
-        Ok(())
+        self.incremental = incremental;
+        Ok(leaves)
     }
 }
