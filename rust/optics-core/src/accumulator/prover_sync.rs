67ea03b949e2038f9feb9c2456084d4464df6c7d
Luke Tchang <ltchang@stanford.edu>
2021-02-17 08:31:11 -0800

    refactor: cleans up error handling

--- rust/optics-core/src/accumulator/prover_sync.rs
+++ rust/optics-core/src/accumulator/prover_sync.rs
@@ -27,7 +27,7 @@ pub struct ProverSync {
 #[derive(Debug, thiserror::Error)]
 pub enum ProverSyncError {
     /// Local tree up-to-date but root does not match signed update"
-    #[error("Local tree up-to-date but root does not match update. Local root: {local_root}. Update root: {new_root}.")]
+    #[error("Local tree up-to-date but root does not match update. Local root: {local_root}. Update root: {new_root}. WARNING: this could indicate malicious updater or long reorganiation process!")]
     MismatchedRoots {
         /// Root of prover's local merkle tree
         local_root: H256,
@@ -51,24 +51,14 @@ impl ProverSync {
     async fn poll_updates(&mut self, interval_seconds: u64) -> Result<(), ProverSyncError> {
         let mut interval = interval(Duration::from_secs(interval_seconds));
 
-        let mut num_retries = self.num_retries.clone();
         loop {
             let local_root = self.prover.read().await.root();
 
-            let signed_update_res = self.home.signed_update_by_old_root(local_root).await;
+            let signed_update_opt = self.home.signed_update_by_old_root(local_root)
+                .await
+                .map_err(|e| ProverSyncError::ChainCommunicationError(e))?;
 
-            // If error polling signed update, log error and try again
-            if let Err(e) = signed_update_res {
-                if num_retries <= 0 {
-                    return Err(ProverSyncError::ChainCommunicationError(e));
-                }
-                
-                tracing::error!("Error retrieving signed_update: {}", e);
-                num_retries -= 1;
-                continue;
-            }
-
-            if let Some(signed_update) = signed_update_res.unwrap() {
+            if let Some(signed_update) = signed_update_opt {
                 self.update_prover_tree(local_root, signed_update.update.new_root)
                     .await?;
             }
@@ -121,24 +111,13 @@ impl ProverSync {
         let mut incremental = self.incremental.clone();
         let mut local_root = local_root;
 
-        let mut num_retries = self.num_retries.clone();
         while local_root != new_root {
             let tree_size = incremental.count();
-            let leaf_res = self.home.leaf_by_tree_index(tree_size).await;
+            let leaf_opt = self.home.leaf_by_tree_index(tree_size)
+                .await
+                .map_err(|e| ProverSyncError::ChainCommunicationError(e))?;
 
-            // If error retrieving leaf, log error and try again
-            if let Err(e) = leaf_res {
-                if num_retries <= 0 {
-                    return Err(ProverSyncError::ChainCommunicationError(e));
-                }
-
-                tracing::error!("Error retrieving leaf at tree_size {}: {}", tree_size, e);
-                num_retries -= 1;
-                continue;
-            }
-
-            // If new leaf exists, try to ingest into incremental
-            if let Some(leaf) = leaf_res.unwrap() {
+            if let Some(leaf) = leaf_opt {
                 incremental.ingest(leaf);
                 leaves.push(leaf);
                 local_root = incremental.root();
