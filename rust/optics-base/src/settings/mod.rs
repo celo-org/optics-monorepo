4bbd81fa10376cdac9fc6da0b3e1de6998c2222c
Erin Hales <mcbridee093@gmail.com>
2021-02-22 11:05:54 -0700

    fix: serde Deserialize

--- rust/optics-base/src/settings/mod.rs
+++ rust/optics-base/src/settings/mod.rs
@@ -1,6 +1,7 @@
 use color_eyre::Report;
 use config::{Config, ConfigError, Environment, File};
 use std::{collections::HashMap, env};
+use serde::Deserialize;
 
 use optics_core::traits::{Home, Replica};
 
@@ -17,7 +18,7 @@ use log::TracingConfig;
 ///
 /// Specify the chain name (enum variant) in toml under the `chain` key
 /// Specify the connection details as a toml object under the `connection` key.
-#[derive(Debug, serde::Deserialize)]
+#[derive(Debug, Deserialize)]
 #[serde(tag = "rpc-style", content = "config", rename_all = "kebab-case")]
 pub enum ChainConf {
     /// Ethereum configuration
@@ -26,7 +27,7 @@ pub enum ChainConf {
 
 /// A chain setup is a domain ID, an address on that chain (where the home or
 /// replica is deployed) and details for connecting to the chain API.
-#[derive(Debug, serde::Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct ChainSetup {
     domain: u32,
     address: String,
@@ -60,26 +61,26 @@ impl ChainSetup {
 /// follows:
 ///
 /// ```
-/// use optics_base::settings::*;
-///
-/// pub struct OtherSettings { /* anything */ };
-///
-/// #[derive(Debug, serde::Deseirialize)]
-/// pub struct MySettings {
-///     #[serde(flatten)]
-///     base_settings: Settings,
-///     #[serde(flatten)]
-///     other_settings: (),
-/// }
-///
-/// // Make sure to define MySettings::new()
-/// impl MySettings {
-///     fn new() -> Self {
-///         unimplemented!()
-///     }
-/// }
+// / use optics_base::settings::*;
+// /
+// / pub struct OtherSettings { /* anything */ };
+// /
+// / #[derive(Debug, Deseirialize)]
+// / pub struct MySettings {
+// /     #[serde(flatten)]
+// /     base_settings: Settings,
+// /     #[serde(flatten)]
+// /     other_settings: (),
+// / }
+// /
+// / // Make sure to define MySettings::new()
+// / impl MySettings {
+// /     fn new() -> Self {
+// /         unimplemented!()
+// /     }
+// / }
 /// ```
-#[derive(Debug, serde::Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct Settings {
     /// The home configuration
     pub home: ChainSetup,
