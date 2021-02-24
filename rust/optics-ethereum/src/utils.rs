d4d1f28d3fe7b334fc62196dc931766b839a73e8
Luke Tchang <ltchang@stanford.edu>
2021-02-24 13:47:08 -0800

    test: adds Rust destinationAndSequence test to output vector

--- rust/optics-ethereum/src/utils.rs
+++ rust/optics-ethereum/src/utils.rs
@@ -3,5 +3,36 @@ pub(crate) fn destination_and_sequence(destination: u32, sequence: u32) -> u64 {
     ((destination as u64) << 32) & sequence as u64
 }
 
-// TODO: add test to ensure calculation matches Solidity
-// calcdestinationAndSequence function
+#[cfg(test)]
+mod test {
+    use serde_json::{json, Value};
+
+    use super::*;
+    use std::{fs::{OpenOptions}, io::Write};
+
+    #[test]
+    // Outputs combined destination and sequence test cases in /vector/
+    // destinationSequenceTestCases.json
+    fn output_destination_and_sequences() {
+        let test_cases: Vec<Value> = (1..=5)
+            .map(|i| json!({
+                "destination": i * 100,
+                "sequence": i * 500,
+                "expectedDestinationAndSequence": destination_and_sequence(i * 100, i * 500)
+            }))
+            .collect();
+
+        let json = json!({
+            "testCases": test_cases
+        }).to_string();
+
+        let mut file = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .truncate(true)
+            .open("../../vectors/destinationSequenceTestCases.json")
+            .expect("Failed to open/create file");
+
+        file.write_all(json.as_bytes()).expect("Failed to write to file");
+    }
+}
