71c19055853cbd70fccf992b0920bde53559c679
Erin Hales <mcbridee093@gmail.com>
2021-02-26 11:52:56 -0700

    enhance: pre-commit.sh

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,8 +1,25 @@
+#!/bin/sh
+
+abort()
+{
+    echo >&2 '
+***************
+*** ABORTED ***
+***************
+'
+    echo "An error occurred. Please review your code and try again" >&2
+    exit 1
+}
+
+trap 'abort' 0
+
 set -e
 cd solidity/optics-core/
-npm test && npm run prettier && npm run lint
+npm test
+npm run lint
 cd ../optics-bridge/
-npm test && npm run prettier && npm run lint
+npm test
+npm run lint
 
 cd ../../rust/
 echo '+cargo test'
@@ -12,3 +29,13 @@ cargo clippy -- -D warnings
 echo '+cargo fmt -- --check'
 cargo fmt -- --check
 cd ..
+
+trap : 0
+
+trap : 0
+
+echo >&2 '
+************
+*** DONE *** 
+************
+'
