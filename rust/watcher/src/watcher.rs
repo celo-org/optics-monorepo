b80639a863deb83a7441ace6c011da8838cf1b92
Luke Tchang <ltchang@stanford.edu>
2021-03-08 11:40:19 -0800

    test: adds history sync update history test

--- rust/watcher/src/watcher.rs
+++ rust/watcher/src/watcher.rs
@@ -49,13 +49,13 @@ where
 {
     pub fn new(
         interval_seconds: u64,
-        current_root: H256,
+        from: H256,
         tx: mpsc::Sender<SignedUpdate>,
         contract: Arc<C>,
     ) -> Self {
         Self {
             interval_seconds,
-            current_root,
+            current_root: from,
             tx,
             contract,
         }
@@ -101,7 +101,7 @@ where
     C: Common + ?Sized + 'static,
 {
     interval_seconds: u64,
-    from: H256,
+    current_root: H256,
     tx: mpsc::Sender<SignedUpdate>,
     contract: Arc<C>,
 }
@@ -117,7 +117,7 @@ where
         contract: Arc<C>,
     ) -> Self {
         Self {
-            from,
+            current_root: from,
             tx,
             contract,
             interval_seconds,
@@ -129,7 +129,10 @@ where
     }
 
     async fn update_history(&mut self) -> Result<()> {
-        let previous_update = self.contract.signed_update_by_new_root(self.from).await?;
+        let previous_update = self
+            .contract
+            .signed_update_by_new_root(self.current_root)
+            .await?;
 
         if previous_update.is_none() {
             // Task finished
@@ -140,9 +143,9 @@ where
         let previous_update = previous_update.unwrap();
 
         // set up for next loop iteration
-        self.from = previous_update.update.previous_root;
+        self.current_root = previous_update.update.previous_root;
         self.tx.send(previous_update).await?;
-        if self.from.is_zero() {
+        if self.current_root.is_zero() {
             // Task finished
             return Err(Report::new(WatcherError::SyncingFinished));
         }
@@ -414,6 +417,86 @@ mod test {
 
             assert_eq!(contract_watcher.current_root, second_root);
             assert_eq!(rx.recv().await.unwrap(), signed_update);
+        }
+
+        let mock_home = Arc::get_mut(&mut home).unwrap();
+        mock_home.checkpoint();
+    }
+
+    #[tokio::test]
+    async fn history_sync_updates_history() {
+        let signer: LocalWallet =
+            "1111111111111111111111111111111111111111111111111111111111111111"
+                .parse()
+                .unwrap();
+
+        let zero_root = H256::zero(); // Original zero root
+        let first_root = H256::from([1; 32]);
+        let second_root = H256::from([2; 32]);
+
+        // Zero root to first root
+        let first_signed_update = Update {
+            origin_domain: 1,
+            previous_root: zero_root,
+            new_root: first_root,
+        }
+        .sign_with(&signer)
+        .await
+        .expect("!sign");
+
+        // First root to second root
+        let second_signed_update = Update {
+            origin_domain: 1,
+            previous_root: first_root,
+            new_root: second_root,
+        }
+        .sign_with(&signer)
+        .await
+        .expect("!sign");
+
+        let mut mock_home = MockHomeContract::new();
+        {
+            let first_signed_update = first_signed_update.clone();
+            let second_signed_update = second_signed_update.clone();
+            // home.signed_update_by_new_root called once with second_root
+            // and returns mock value second_signed_update
+            mock_home
+                .expect__signed_update_by_new_root()
+                .withf(move |r: &H256| *r == second_root)
+                .times(1)
+                .return_once(move |_| Ok(Some(second_signed_update)));
+            // home.signed_update_by_new_root called once with first_root
+            // and returns mock value first_signed_update
+            mock_home
+                .expect__signed_update_by_new_root()
+                .withf(move |r: &H256| *r == first_root)
+                .times(1)
+                .return_once(move |_| Ok(Some(first_signed_update)));
+        }
+
+        let mut home: Arc<Homes> = Arc::new(mock_home.into());
+        let (tx, mut rx) = mpsc::channel(200);
+        {
+            let mut history_sync = HistorySync::new(3, second_root, tx.clone(), home.clone());
+
+            // First update_history call returns first -> second update
+            history_sync
+                .update_history()
+                .await
+                .expect("Should have received Ok(())");
+
+            assert_eq!(history_sync.current_root, first_root);
+            assert_eq!(rx.recv().await.unwrap(), second_signed_update);
+
+            // Second update_history call returns zero -> first update
+            // and should return WatcherError::SyncingFinished
+            history_sync
+                .update_history()
+                .await
+                .expect_err("Should have received WatcherError::SyncingFinished");
+
+            assert_eq!(history_sync.current_root, zero_root);
+            assert_eq!(rx.recv().await.unwrap(), first_signed_update)
         }
 
         let mock_home = Arc::get_mut(&mut home).unwrap();
