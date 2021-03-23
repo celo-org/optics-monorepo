42225ca9890f9a481caba07851cc55078efd4439
Erin Hales <mcbridee093@gmail.com>
2021-03-23 15:54:57 -0600

    clean: more explicit variable name

--- solidity/optics-governance/contracts/GovernanceRouter.sol
+++ solidity/optics-governance/contracts/GovernanceRouter.sol
@@ -52,11 +52,11 @@ contract GovernanceRouter is OpticsHandlerI {
 
         // immutable state variables cannot be accessed within the constructor, so we set this variable for use
         // in _transferGovernor
-        uint32 d = usingOptics.originDomain();
-        localDomain = d;
+        uint32 _localDomain = usingOptics.originDomain();
+        localDomain = _localDomain;
         bool _isLocalDomain = true;
 
-        _transferGovernor(d, _governor, _isLocalDomain);
+        _transferGovernor(_localDomain, _governor, _isLocalDomain);
 
         setUsingOptics(_usingOptics);
     }
