8091f5739a1f6999bc2979750dba8c7c7be417c5
Anna Carroll <anna-carroll@users.noreply.github.com>
2021-03-30 20:03:54 +0200

    Migrate from `constructor` to `initialize()` (#195)
    * Refactor constructors to initialize
    
    * Fix merge, fix lint
    
    * Move ownDomain to initialize; logically paired with instance, not implementation
    
    * Add initialize to Common
    
    * Solidity tests passing
    
    * Migrate GovernanceRouter to initialize
    
    * Fix GovernanceRouter
    
    * Integrate Replica and ProcessingReplica; fix function collision in abigen
    
    * Fix: rust syntax; remove commented line
    
    * Gate initialize() on state.UNINITIALIZED
    
    * Rename queue.init() -> queue.initialize()

--- solidity/optics-core/scripts/update_abis.sh
+++ solidity/optics-core/scripts/update_abis.sh
@@ -1,2 +1,2 @@
-cat artifacts/contracts/Replica.sol/ProcessingReplica.json| jq .abi > ../../abis/ProcessingReplica.abi.json && \
+cat artifacts/contracts/Replica.sol/Replica.json| jq .abi > ../../abis/Replica.abi.json && \
 cat artifacts/contracts/Home.sol/Home.json| jq .abi > ../../abis/Home.abi.json
