384140ca96fd65003e06045a18248d5a4e5a623f
anna-caroll <anna.s.carroll@gmail.com>
2021-03-31 16:03:32 +0200

    Remove deploy functionality from UpgradeBeacon - not necessary interface

--- solidity/optics-core/contracts/upgrade/UpgradeBeaconController.sol
+++ solidity/optics-core/contracts/upgrade/UpgradeBeaconController.sol
@@ -9,38 +9,14 @@ import "./UpgradeBeacon.sol";
  * @title UpgradeBeaconController
  *
  * @notice
- * This contract is capable of deploying UpgradeBeacon contracts which it controls
- * and changing the Implementation stored at an UpgradeBeacon which it controls
+ * This contract is capable of changing the Implementation address
+ * stored at any UpgradeBeacon which it controls
  *
  * This implementation is a minimal version inspired by 0age's implementation:
  * https://github.com/dharma-eng/dharma-smart-wallet/blob/master/contracts/upgradeability/DharmaUpgradeBeaconController.sol
  */
 contract UpgradeBeaconController is Ownable {
-    event BeaconDeployed(address beacon, address implementation);
     event BeaconUpgraded(address indexed beacon, address implementation);
-
-    /**
-     * @notice Deploy the Controller and the initial set of UpgradeBeacon
-     * contracts using this Controller, each with their own initial Implementations
-     *
-     * @param _implementations - Array of addresses of Implementation contracts
-     * for which we want to deploy an UpgradeBeacon with that Implementation
-     */
-    constructor(address[] memory _implementations) {
-        for (uint256 i = 0; i < _implementations.length; i++) {
-            _deployUpgradeBeacon(_implementations[i]);
-        }
-    }
-
-    /**
-     * @notice Deploy a new UpgradeBeacon contract using this as the Controller
-     * with the initial implementation provided
-     *
-     * @param _implementation - Address of the initial Implementation for the new Beacon
-     */
-    function deployUpgradeBeacon(address _implementation) public onlyOwner {
-        _deployUpgradeBeacon(_implementation);
-    }
 
     /**
      * @notice Modify the Implementation stored in the UpgradeBeacon,
@@ -69,19 +45,5 @@ contract UpgradeBeaconController is Ownable {
         }
 
         emit BeaconUpgraded(_beacon, _implementation);
-    }
-
-    /**
-     * @notice Deploy a new UpgradeBeacon contract using this as the Controller
-     * with the initial implementation provided
-     *
-     * @param _implementation - Address of the initial Implementation for the new Beacon
-     */
-    function _deployUpgradeBeacon(address _implementation) private {
-        // Deploy the beacon with this as the controller
-        UpgradeBeacon _beacon =
-            new UpgradeBeacon(_implementation, address(this));
-
-        emit BeaconDeployed(address(_beacon), _implementation);
     }
 }
