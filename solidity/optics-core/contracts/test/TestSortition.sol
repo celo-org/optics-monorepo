9fe2f79cab573dcb4147d8c7411f57aac52436f1
Erin Hales <mcbridee093@gmail.com>
2021-03-31 10:35:01 -0600

    refactor: remove sortition, update UpdateManager

--- solidity/optics-core/contracts/test/TestSortition.sol
+++ solidity/optics-core/contracts/test/TestSortition.sol
@@ -1,33 +1,4 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 pragma solidity >=0.6.11;
 
-import {SortitionI} from "../../interfaces/SortitionI.sol";
-
-contract TestSortition is SortitionI {
-    address internal updater;
-    address internal home;
-
-    event Slashed();
-
-    constructor(address _updater) payable {
-        updater = _updater;
-    }
-
-    function setHome(address _home) external {
-        home = _home;
-    }
-
-    function setUpdater(address _updater) external {
-        updater = _updater;
-    }
-
-    function current() external view override returns (address) {
-        return updater;
-    }
-
-    // solhint-disable-next-line no-unused-vars
-    function slash(address payable _reporter) external override {
-        require(msg.sender == home, "!home");
-        emit Slashed();
-    }
-}
+// TODO: update to TestUpdaterManager
