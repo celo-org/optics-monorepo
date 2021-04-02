6610def0c968072dd446b33b780ff878564d8ae3
Luke Tchang <ltchang@stanford.edu>
2021-04-02 11:08:20 -0700

    feature: adds signer enum variants

--- rust/optics-ethereum/src/lib.rs
+++ rust/optics-ethereum/src/lib.rs
@@ -17,3 +17,22 @@ mod utils;
 
 #[cfg(not(doctest))]
 pub use crate::{home::EthereumHome, replica::EthereumReplica};
+
+use ethers::signers::LocalWallet;
+
+#[cfg(feature = "ledger")]
+use ethers::signers::Ledger;
+#[cfg(feature = "yubi")]
+use ethers::signers::YubiWallet;
+
+/// Ethereum-supported signer types
+pub enum Signers {
+    /// A wallet instantiated with a locally stored private key
+    Local(LocalWallet),
+    /// A wallet instantiated with a YubiHSM
+    #[cfg(feature = "ledger")]
+    Ledger(Ledger),
+    /// A wallet instantiated with a Ledger
+    #[cfg(feature = "yubi")]
+    Yubi(YubiWallet),
+}
