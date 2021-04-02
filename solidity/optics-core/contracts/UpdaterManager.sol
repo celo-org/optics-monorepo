f91e5fc903c26863f95b2489e8da316c4d408446
Erin Hales <mcbridee093@gmail.com>
2021-04-02 11:29:55 -0600

    enhance: emit NewHome event, docs

--- solidity/optics-core/contracts/UpdaterManager.sol
+++ solidity/optics-core/contracts/UpdaterManager.sol
@@ -12,27 +12,50 @@ contract UpdaterManager is UpdaterManagerI, Ownable {
     address internal updater;
     address internal home;
 
+    /// @notice Event emitted when the updater is slashed
     event Slashed();
+
+    /**
+     * @notice Event emitted when a new home is set
+     * @param home The address of the new home
+     */
+    event NewHome(address home);
 
     constructor(address _updater) payable Ownable() {
         updater = _updater;
     }
 
+    /**
+     * @notice Permissioned function that sets the address of the new home contract
+     * @param _home The address of the new home contract
+     */
     function setHome(address _home) external onlyOwner {
         require(Address.isContract(_home), "!home");
         home = _home;
+
+        emit NewHome(_home);
     }
 
+    /**
+     * @notice Permissioned function that sets the address of the new updater contract
+     * @param _updater The address of the new updater contract
+     */
     function setUpdater(address _updater) external onlyOwner {
         updater = _updater;
         Home homeContract = Home(home);
         homeContract.setUpdater(_updater);
     }
 
+    /// @notice Returns the address of the current updater
     function current() external view override returns (address) {
         return updater;
     }
 
+    /**
+     * @notice Slashes the updater
+     * @dev Currently only emits Slashed event, functionality will come later
+     * @param _reporter The address of the entity that reported the updater fraud
+     */
     // solhint-disable-next-line no-unused-vars
     function slash(address payable _reporter) external override {
         require(msg.sender == home, "!home");
