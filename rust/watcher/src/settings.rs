4ba0346ae56f76ef019e9e0f83b37b6fb161260e
Luke Tchang <ltchang@stanford.edu>
2021-04-12 09:36:23 -0700

    feature: adds xapp interface enum to optics-base

--- rust/watcher/src/settings.rs
+++ rust/watcher/src/settings.rs
@@ -1,10 +1,11 @@
 //! Configuration
 
-use optics_base::decl_settings;
+use optics_base::{decl_settings, settings::ChainSetup};
 use optics_ethereum::EthereumSigner;
 
 decl_settings!(Settings {
     agent: "watcher",
     watcher: EthereumSigner,
+    xapp_connection_manager: ChainSetup,
     polling_interval: u64,
 });
