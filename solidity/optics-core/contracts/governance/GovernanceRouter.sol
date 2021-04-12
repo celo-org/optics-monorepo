f466ac2cebe136e735122f4f7d7c87efa9add3e3
Erin Hales <mcbridee093@gmail.com>
2021-04-12 14:22:00 -0600

    refactor: reorder and add documentation GovernanceRouter

--- solidity/optics-core/contracts/governance/GovernanceRouter.sol
+++ solidity/optics-core/contracts/governance/GovernanceRouter.sol
@@ -79,51 +79,19 @@ contract GovernanceRouter is IMessageRecipient {
         _;
     }
 
-    /*
-    --- DOMAIN/ADDRESS VALIDATION HELPERS  ---
-    */
-    function setXAppConnectionManager(address _xAppConnectionManager)
-        public
-        onlyGovernor
-    {
-        xAppConnectionManager = XAppConnectionManager(_xAppConnectionManager);
-    }
-
-    function isGovernorRouter(uint32 _domain, bytes32 _address)
-        internal
-        view
-        returns (bool _isGovernorRouter)
-    {
-        _isGovernorRouter =
-            _domain == governorDomain &&
-            _address == routers[_domain];
-    }
-
-    function mustHaveRouter(uint32 _domain)
-        internal
-        view
-        returns (bytes32 _router)
-    {
-        _router = routers[_domain];
-        require(_router != bytes32(0), "!router");
-    }
-
-    function isLocalDomain(uint32 _domain)
-        internal
-        view
-        returns (bool _isLocalDomain)
-    {
-        _isLocalDomain = _domain == localDomain;
-    }
-
-    /*
-    --- MESSAGE HANDLING ---
-        for all non-Governor chains to handle messages
-        sent from the Governor chain via Optics
-        --
-        Governor chain should never receive messages,
-        because non-Governor chains are not able to send them
-    */
+    /**
+     * @notice Message handler
+     *
+     * For all non-Governor chains to handle messages
+     * sent from the Governor chain via Optics.
+     *
+     * Governor chain should never receive messages,
+     * because non-Governor chains are not able to send them
+     * @param _origin The domain (of the Governor Router)
+     * @param _sender The sender
+     * @param _message The message
+     * @return _ret
+     */
     function handle(
         uint32 _origin,
         bytes32 _sender,
@@ -148,75 +116,39 @@ contract GovernanceRouter is IMessageRecipient {
         require(false, "!valid message type");
     }
 
-    function handleCall(bytes29 _msg)
-        internal
-        typeAssert(_msg, GovernanceMessage.Types.Call)
-        returns (bytes memory _ret)
-    {
-        GovernanceMessage.Call[] memory _calls = _msg.getCalls();
-        for (uint256 i = 0; i < _calls.length; i++) {
-            _dispatchCall(_calls[i]);
-        }
-
-        return hex"";
-    }
-
-    function handleTransferGovernor(bytes29 _msg)
-        internal
-        typeAssert(_msg, GovernanceMessage.Types.TransferGovernor)
-        returns (bytes memory _ret)
-    {
-        uint32 _newDomain = _msg.domain();
-        address _newGovernor = TypeCasts.bytes32ToAddress(_msg.governor());
-
-        bool _isLocalDomain = isLocalDomain(_newDomain);
-
-        _transferGovernor(_newDomain, _newGovernor, _isLocalDomain);
-
-        return hex"";
-    }
-
-    function handleEnrollRouter(bytes29 _msg)
-        internal
-        typeAssert(_msg, GovernanceMessage.Types.EnrollRouter)
-        returns (bytes memory _ret)
-    {
-        uint32 _domain = _msg.domain();
-        bytes32 _router = _msg.router();
-
-        _enrollRouter(_domain, _router);
-
-        return hex"";
-    }
-
-    /*
-    --- MESSAGE DISPATCHING ---
-        for the Governor chain to send messages
-        to other chains via Optics
-        --
-        functionality not accessible on non-Governor chains
-        (governor is set to 0x0 on non-Governor chains)
-    */
-
-    function callLocal(GovernanceMessage.Call[] calldata calls)
+    /**
+     * @notice Dispatches local call
+     * @param _calls The calls
+     */
+    function callLocal(GovernanceMessage.Call[] calldata _calls)
         external
         onlyGovernor
     {
-        for (uint256 i = 0; i < calls.length; i++) {
-            _dispatchCall(calls[i]);
+        for (uint256 i = 0; i < _calls.length; i++) {
+            _dispatchCall(_calls[i]);
         }
     }
 
+    /**
+     * @notice Dispatches call to remote router
+     * @param _destination The destination router
+     * @param _calls The calls
+     */
     function callRemote(
         uint32 _destination,
-        GovernanceMessage.Call[] calldata calls
+        GovernanceMessage.Call[] calldata _calls
     ) external onlyGovernor {
         bytes32 _router = mustHaveRouter(_destination);
-        bytes memory _msg = GovernanceMessage.formatCalls(calls);
+        bytes memory _msg = GovernanceMessage.formatCalls(_calls);
 
         Home(xAppConnectionManager.home()).enqueue(_destination, _router, _msg);
     }
 
+    /**
+     * @notice Transfer governorship to a new domain
+     * @param _newDomain The domain of the new governor
+     * @param _newGovernor The address of the new governor
+     */
     function transferGovernor(uint32 _newDomain, address _newGovernor)
         external
         onlyGovernor
@@ -240,6 +172,12 @@ contract GovernanceRouter is IMessageRecipient {
         _sendToAllRemoteRouters(transferGovernorMessage);
     }
 
+    /**
+     * @notice Enrolls a new router for a given domain and dispatches to all
+     * remote routers
+     * @param _domain The domain
+     * @param _router The new router
+     */
     function enrollRouter(uint32 _domain, bytes32 _router)
         external
         onlyGovernor
@@ -252,6 +190,94 @@ contract GovernanceRouter is IMessageRecipient {
         _sendToAllRemoteRouters(enrollRouterMessage);
     }
 
+    /**
+     * @notice Enrolls a router locally
+     *
+     * @dev External helper for contract setup.
+     *
+     * convenience function so deployer can setup the router mapping for the
+     * contract locally before transferring to a new domain to the remote governor.
+     * @param _domain The domain
+     * @param _router The new router
+     */
+    function enrollRouterSetup(uint32 _domain, bytes32 _router)
+        external
+        onlyGovernor
+    {
+        _enrollRouter(_domain, _router); // enroll the router locally
+    }
+
+    /**
+     * @notice Sets up XAppConnectionManager
+     * @dev Domain/address validation helper
+     * @param _xAppConnectionManager The address of the new xAppConnectionManager
+     */
+    function setXAppConnectionManager(address _xAppConnectionManager)
+        public
+        onlyGovernor
+    {
+        xAppConnectionManager = XAppConnectionManager(_xAppConnectionManager);
+    }
+
+    /**
+     * @notice Handles dispatching a call
+     * @param _msg The message
+     * @return _ret
+     */
+    function handleCall(bytes29 _msg)
+        internal
+        typeAssert(_msg, GovernanceMessage.Types.Call)
+        returns (bytes memory _ret)
+    {
+        GovernanceMessage.Call[] memory _calls = _msg.getCalls();
+        for (uint256 i = 0; i < _calls.length; i++) {
+            _dispatchCall(_calls[i]);
+        }
+
+        return hex"";
+    }
+
+    /**
+     * @notice Handles transfer to a new Governor
+     * @param _msg The message
+     * @return _ret
+     */
+    function handleTransferGovernor(bytes29 _msg)
+        internal
+        typeAssert(_msg, GovernanceMessage.Types.TransferGovernor)
+        returns (bytes memory _ret)
+    {
+        uint32 _newDomain = _msg.domain();
+        address _newGovernor = TypeCasts.bytes32ToAddress(_msg.governor());
+        bool _isLocalDomain = isLocalDomain(_newDomain);
+
+        _transferGovernor(_newDomain, _newGovernor, _isLocalDomain);
+
+        return hex"";
+    }
+
+    /**
+     * @notice Handles enrolling a router
+     * @param _msg The message
+     * @return _ret
+     */
+    function handleEnrollRouter(bytes29 _msg)
+        internal
+        typeAssert(_msg, GovernanceMessage.Types.EnrollRouter)
+        returns (bytes memory _ret)
+    {
+        uint32 _domain = _msg.domain();
+        bytes32 _router = _msg.router();
+
+        _enrollRouter(_domain, _router);
+
+        return hex"";
+    }
+
+    /**
+     * @notice Dispatches message to all remote routers
+     * @param _msg The message
+     */
     function _sendToAllRemoteRouters(bytes memory _msg) internal {
         Home home = Home(xAppConnectionManager.home());
 
@@ -262,12 +288,11 @@ contract GovernanceRouter is IMessageRecipient {
         }
     }
 
-    /*
-    --- ACTIONS IMPLEMENTATION ---
-        implementations of local state changes
-        performed when handling AND dispatching messages
-    */
-
+    /**
+     * @notice Dispatches call
+     * @param _call The call
+     * @return _ret
+     */
     function _dispatchCall(GovernanceMessage.Call memory _call)
         internal
         returns (bytes memory _ret)
@@ -280,6 +305,11 @@ contract GovernanceRouter is IMessageRecipient {
         require(_success, "call failed");
     }
 
+    /**
+     * @notice Transfers governorship to a new domain
+     * @param _newDomain The new domain
+     * @param _newGovernor The address of the new governor
+     */
     function _transferGovernor(
         uint32 _newDomain,
         address _newGovernor,
@@ -299,6 +329,11 @@ contract GovernanceRouter is IMessageRecipient {
         governor = _governor;
     }
 
+    /**
+     * @notice Enrolls a new router for a given domain
+     * @param _domain The domain
+     * @param _newRouter The new router
+     */
     function _enrollRouter(uint32 _domain, bytes32 _newRouter) internal {
         bytes32 _previousRouter = routers[_domain];
 
@@ -316,10 +351,18 @@ contract GovernanceRouter is IMessageRecipient {
         routers[_domain] = _newRouter;
     }
 
+    /**
+     * @notice Adds a given domain
+     * @param _domain The domain
+     */
     function _addDomain(uint32 _domain) internal {
         domains.push(_domain);
     }
 
+    /**
+     * @notice Removes a domain and its associated router
+     * @param _domain The domain
+     */
     function _removeDomain(uint32 _domain) internal {
         delete routers[_domain];
 
@@ -332,16 +375,47 @@ contract GovernanceRouter is IMessageRecipient {
         }
     }
 
-    /*
-    --- EXTERNAL HELPER FOR CONTRACT SETUP ---
-        convenience function so deployer can setup the router mapping for the contract locally
-        before transferring governorship to the remote governor
-    */
-
-    function enrollRouterSetup(uint32 _domain, bytes32 _router)
-        external
-        onlyGovernor
+    /**
+     * @notice Determines if a given domain and address is the Governor Router
+     * @param _domain The domain
+     * @param _address The address of the domain's router
+     * @return _isGovernorRouter True if the given domain/address is the
+     * Governor Router.
+     */
+    function isGovernorRouter(uint32 _domain, bytes32 _address)
+        internal
+        view
+        returns (bool _isGovernorRouter)
     {
-        _enrollRouter(_domain, _router); // enroll the router locally
+        _isGovernorRouter =
+            _domain == governorDomain &&
+            _address == routers[_domain];
+    }
+
+    /**
+     * @notice Requires that a domain have a router and returns the router
+     * @param _domain The domain
+     * @return _router - The domain's router
+     */
+    function mustHaveRouter(uint32 _domain)
+        internal
+        view
+        returns (bytes32 _router)
+    {
+        _router = routers[_domain];
+        require(_router != bytes32(0), "!router");
+    }
+
+    /**
+     * @notice Determines if a given domain is the local domain
+     * @param _domain The domain
+     * @return _isLocalDomain - True if the given domain is the local domain
+     */
+    function isLocalDomain(uint32 _domain)
+        internal
+        view
+        returns (bool _isLocalDomain)
+    {
+        _isLocalDomain = _domain == localDomain;
     }
 }
