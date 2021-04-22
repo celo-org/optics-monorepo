40f427fef1f5fc2397462b47d8daad090a2d6360
Luke Tchang <ltchang@stanford.edu>
2021-04-21 18:56:40 -0700

    test: adds success case transfer governor test

--- solidity/optics-core/contracts/test/TestGovernanceRouter.sol
+++ solidity/optics-core/contracts/test/TestGovernanceRouter.sol
@@ -6,4 +6,13 @@ import "../governance/GovernanceRouter.sol";
 
 contract TestGovernanceRouter is GovernanceRouter {
     constructor(uint32 _localDomain) GovernanceRouter(_localDomain) {} // solhint-disable-line no-empty-blocks
+
+    function testSetRouter(uint32 _domain, bytes32 _router) external {
+        _setRouter(_domain, _router); // set the router locally
+
+        bytes memory _setRouterMessage =
+            GovernanceMessage.formatSetRouter(_domain, _router);
+
+        _sendToAllRemoteRouters(_setRouterMessage);
+    }
 }
