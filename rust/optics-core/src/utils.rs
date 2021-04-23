84076dba88ef68581caed17218af05d4b8f6fee7
Erin Hales <mcbridee093@gmail.com>
2021-04-23 09:34:29 -0600

    refactor: rename domain_hash

--- rust/optics-core/src/utils.rs
+++ rust/optics-core/src/utils.rs
@@ -2,10 +2,10 @@ use ethers::core::types::H256;
 use sha3::{Digest, Keccak256};
 
 /// Computes hash of domain concatenated with "OPTICS"
-pub(crate) fn domain_hash(domain: u32) -> H256 {
+pub(crate) fn home_domain_hash(home_domain: u32) -> H256 {
     H256::from_slice(
         Keccak256::new()
-            .chain(domain.to_be_bytes())
+            .chain(home_domain.to_be_bytes())
             .chain("OPTICS".as_bytes())
             .finalize()
             .as_slice(),
--- rust/optics-test/src/mocks/home.rs
+++ rust/optics-test/src/mocks/home.rs
@@ -17,7 +17,7 @@ mock! {
         // Home
         pub fn _local_domain(&self) -> u32 {}
 
-        pub fn _domain_hash(&self) -> H256 {}
+        pub fn _signature_domain(&self) -> H256 {}
 
         pub fn _raw_message_by_sequence(
             &self,
@@ -91,8 +91,8 @@ impl Home for MockHomeContract {
         self._local_domain()
     }
 
-    fn domain_hash(&self) -> H256 {
-        self._domain_hash()
+    fn signature_domain(&self) -> H256 {
+        self._signature_domain()
     }
 
     async fn raw_message_by_sequence(
