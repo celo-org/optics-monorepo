95347c998cbfea12c428cb347ae4801911036813
Luke Tchang <ltchang@stanford.edu>
2021-04-27 11:27:34 -0700

    feature: adds rust output bins to precommit

--- pre-commit.sh
+++ pre-commit.sh
@@ -20,7 +20,36 @@ trap 'abort' 0
 set -e
 git update-index -q --refresh
 
-if ! git diff-index --quiet HEAD -- ./solidity/optics-core; then
+VECTORS_MODIFIED=false
+
+if ! git diff-index --quiet HEAD -- ./rust ./abis; then
+    cd ./rust
+    echo '+cargo test'
+    cargo test
+    echo '+cargo clippy -- -D warnings'
+    cargo clippy -- -D warnings
+    echo '+cargo fmt -- --check'
+    cargo fmt -- --check
+    cd ..
+fi
+
+if ! git diff-index --quiet HEAD -- ./rust/optics-core/src/lib.rs; then
+    cd ./rust/optics-core
+    echo '+cargo run --bin lib_test_output --features output'
+    cargo run --bin lib_test_output --features output
+    cd ../..
+    VECTORS_MODIFIED=true
+fi
+
+if ! git diff-index --quiet HEAD -- ./rust/optics-core/src/utils.rs; then
+    cd ./rust/optics-core
+    echo '+cargo run --bin utils_test_output --features output'
+    cargo run --bin utils_test_output --features output
+    cd ../..
+    VECTORS_MODIFIED=true
+fi
+
+if ! git diff-index --quiet HEAD -- ./solidity/optics-core || [ "$VECTORS_MODIFIED" = true ]; then
     cd solidity/optics-core
     npm test
     npm run lint
@@ -34,20 +63,19 @@ if ! git diff-index --quiet HEAD -- ./solidity/optics-bridge; then
     cd ../..
 fi
 
-if ! git diff-index --quiet HEAD -- ./rust ./abis; then
-    cd ./rust
-    echo '+cargo test'
-    cargo test
-    echo '+cargo clippy -- -D warnings'
-    cargo clippy -- -D warnings
-    echo '+cargo fmt -- --check'
-    cargo fmt -- --check
-    cd ..
+trap : 0
+
+trap : 0
+
+trap : 0
+
+trap : 0
+
+# Only add updated vectors if checks passed
+if [ "$VECTORS_MODIFIED" = true ]; then
+    echo '+git add ./vectors/*'
+    git add ./vectors/*
 fi
-
-trap : 0
-
-trap : 0
 
 echo >&2 '
 ************
