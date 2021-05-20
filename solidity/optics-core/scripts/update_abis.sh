5dd1bbc5a57931f09cc22be4bff5b156376eca5f
Conner Swann <me@connerswann.me>
2021-05-20 15:39:31 -0700

    update_abis script

--- solidity/optics-core/scripts/update_abis.sh
+++ solidity/optics-core/scripts/update_abis.sh
@@ -1,3 +1,7 @@
 cat artifacts/contracts/Replica.sol/Replica.json| jq .abi > ../../abis/Replica.abi.json && \
 cat artifacts/contracts/Home.sol/Home.json| jq .abi > ../../abis/Home.abi.json && \
 cat artifacts/contracts/XAppConnectionManager.sol/XAppConnectionManager.json | jq .abi > ../../abis/XAppConnectionManager.abi.json
+
+cat artifacts/contracts/Replica.sol/Replica.json| jq .abi > ../../rust/abis/Replica.abi.json && \
+cat artifacts/contracts/Home.sol/Home.json| jq .abi > ../../rust/abis/Home.abi.json && \
+cat artifacts/contracts/XAppConnectionManager.sol/XAppConnectionManager.json | jq .abi > ../../rust/abis/XAppConnectionManager.abi.json
