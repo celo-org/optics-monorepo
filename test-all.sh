7a89d748f9b639d1d87ea68a7acd98955721cddc
Erin Hales <mcbridee093@gmail.com>
2021-06-01 14:20:28 -0600

    refactor: test-all script

--- test-all.sh
+++ test-all.sh
@@ -1,34 +1,31 @@
 #!/bin/sh
 
+# update ABIs
+cd ./solidity
+
 echo "+Updating core ABIs"
-cd ./solidity/optics-core
+cd ./optics-core
 npm run compile
-cd ../..
 
 echo "+Updating xapps ABIs"
-cd ./solidity/optics-xapps
+cd ../optics-xapps
 npm run compile
-# add files in case linter modified them
-git add .
-cd ../..
+cd ..
 
-# Conditionally run Rust bins to output into vector JSON files
+# run Rust bins to output into vector JSON files
+cd ../rust/optics-core
+
 echo "+Running lib vector generation"
-cd ./rust/optics-core
 echo '+cargo run --bin lib_test_output --features output'
 cargo run --bin lib_test_output --features output
-cd ../..
 
-# Conditionally run Rust bins to output into vector JSON files
 echo "+Running utils vector generation"
-cd ./rust/optics-core
 echo '+cargo run --bin utils_test_output --features output'
 cargo run --bin utils_test_output --features output
-cd ../..
+cd ..
 
 # Run rust tests, clippy, and formatting
 echo "+Running rust tests"
-cd ./rust
 echo '+cargo fmt -- --check'
 cargo fmt -- --check
 echo '+cargo clippy -- -D warnings'
@@ -37,15 +34,17 @@ echo '+cargo test -- -q'
 cargo test -- -q
 cd ..
 
+# Run solidity tests
+cd ./solidity
+
 # Run solidity/optics-core tests and lint
 echo "+Running optics core tests"
-cd ./solidity/optics-core
+cd ./optics-core
 npm run lint
 npm test
-cd ../..
 
 # Run solidity/optics-xapps tests and lint
 echo "+Running optics-xapps tests"
-cd ./solidity/optics-xapps
+cd ../optics-xapps
 npm run lint
 npm test
