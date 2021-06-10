46831cf883aed2547d8bc6b5d58bb22e150c07ea
Erin Hales <mcbridee093@gmail.com>
2021-06-10 11:00:25 -0600

    refactor: rewrite extendEnvironment in ts

---
+++ typescript/src/lib/types.ts
@@ -0,0 +1,15 @@
+export type Domain = number;
+export type Address = string;
+export type AddressBytes32 = string;
+export type HexString = string;
+
+export type Update = {
+  oldRoot: string,
+  newRoot: string,
+  signature: string,
+}
+
+export type CallData = {
+  to: Address,
+  data: string
+}
