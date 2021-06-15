eadaab5d24596ed8b14080b080e9b2abb657cf7d
Erin Hales <mcbridee093@gmail.com>
2021-06-15 14:06:14 -0600

    build: generate typechain

--- solidity/optics-core/hardhat.config.js
+++ solidity/optics-core/hardhat.config.js
@@ -52,7 +52,7 @@ module.exports = {
     },
   },
   typechain: {
-    outDir: '../../typescript/src/typechain/optics-core',
+    outDir: '../../typescript/typechain/optics-core',
     target: 'ethers-v5',
     alwaysGenerateOverloads: false, // should overloads with full signatures like deposit(uint256) be generated always, even if there are no overloads?
   },
