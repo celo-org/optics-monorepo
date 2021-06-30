b2f7864106cbec96f416b14e88ba10ba7680b053
Erin Hales <mcbridee093@gmail.com>
2021-06-30 14:58:02 -0600

    fix: import conflicts

--- typescript/optics-tests/test/message.test.ts
+++ typescript/optics-tests/test/message.test.ts
@@ -1,8 +1,8 @@
-import { ethers } from 'hardhat';
+import { ethers, optics } from 'hardhat';
 import { expect } from 'chai';
 import { TestMessage, TestMessage__factory } from '../../typechain/optics-core';
 
-import { testCases } from '../../../vectors/messageTestCases.json';
+import testCases from '../../../vectors/message.json';
 
 const remoteDomain = 1000;
 const localDomain = 2000;
