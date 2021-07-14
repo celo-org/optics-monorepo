2505d878104f3ca81e1cb9a3bfebf220936c4293
James Prestwich <james@prestwi.ch>
2021-07-13 21:26:23 -0700

    feature: use TS in both solidity packages

--- solidity/optics-xapps/hardhat.config.ts
+++ solidity/optics-xapps/hardhat.config.ts
@@ -1,14 +1,14 @@
-require('hardhat-gas-reporter');
-require('solidity-coverage');
-require('@nomiclabs/hardhat-waffle');
-require('@typechain/hardhat');
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: {
-    version: '0.7.6',
+    version: "0.7.6",
     settings: {
       optimizer: {
         enabled: true,
@@ -18,18 +18,18 @@ module.exports = {
   },
 
   gasReporter: {
-    currency: 'USD',
+    currency: "USD",
   },
 
   networks: {
     localhost: {
-      url: 'http://localhost:8545',
+      url: "http://localhost:8545",
     },
   },
 
   typechain: {
-    outDir: '../../typescript/typechain/optics-xapps',
-    target: 'ethers-v5',
+    outDir: "../../typescript/typechain/optics-xapps",
+    target: "ethers-v5",
     alwaysGenerateOverloads: false, // should overloads with full signatures like deposit(uint256) be generated always, even if there are no overloads?
   },
 };
