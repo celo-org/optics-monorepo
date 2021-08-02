f93304a479af4b5dcbf6bc323e1f7841ff03d8e6
Erin Hales <mcbridee093@gmail.com>
2021-08-02 13:09:12 -0600

    add: initial attempt at contract verification

--- typescript/optics-tests/hardhat.config.ts
+++ typescript/optics-tests/hardhat.config.ts
@@ -1,9 +1,29 @@
-import '@nomiclabs/hardhat-waffle';
-import './lib/index';
+require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-etherscan');
+require('./lib/index');
+const { task } = require("hardhat/config");
+const { verifyLatestCoreDeploy, verifyLatestBridgeDeploy } = require("../optics-deploy/src/verifyLatestDeploy");
+const dotenv = require('dotenv');
+dotenv.config();
+
+task("verify-latest-deploy", "Verifies the source code of the latest contract deploy")
+    .addParam("type", "The deploy type (`core` or `bridge`)")
+    .setAction(async (args: any) => {
+      const {type} = args;
+      if(type == "core") {
+        await verifyLatestCoreDeploy();
+      } else if (type == "bridge") {
+        await verifyLatestBridgeDeploy();
+      }
+    });
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: '0.7.3',
+
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  }
 };
