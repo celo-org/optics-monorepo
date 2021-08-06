1752453fd91c2a28594de77114d1076a2c5d34a1
Erin Hales <mcbridee093@gmail.com>
2021-08-05 19:12:49 -0600

    refactor: revert old verification code in typescript folder

--- typescript/optics-tests/hardhat.config.ts
+++ typescript/optics-tests/hardhat.config.ts
@@ -1,31 +1,9 @@
-require('@nomiclabs/hardhat-waffle');
-require('@nomiclabs/hardhat-etherscan');
-require('./lib/index');
+import '@nomiclabs/hardhat-waffle';
+import './lib/index';
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: {
-    version: "0.7.6",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 999999,
-      },
-    },
-  },
-
-  networks: {
-    localhost: {
-      url: "http://localhost:8545",
-    },
-    goerli: {
-      url: "https://goerli.infura.io/v3/5c456d7844fa40a683e934df60534c60",
-    },
-    kovan: {
-      url: "https://kovan.infura.io/v3/5c456d7844fa40a683e934df60534c60",
-    },
-    // TODO: add Ropsten, Rinkeby, Mainnet
-  }
+  solidity: "0.7.6",
 };
