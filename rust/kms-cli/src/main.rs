44aa71b0d78fcdf89d6efe07ea0efe0f854044cc
James Prestwich <james@prestwi.ch>
2021-08-10 10:33:29 -0700

    bug: short designator conflict + docs

--- rust/kms-cli/src/main.rs
+++ rust/kms-cli/src/main.rs
@@ -34,30 +34,43 @@ fn init_kms(region: String) {
 #[clap(version = "0.1", author = "James Prestwich")]
 pub struct Opts {
     // TX
+    /// The TX value
     #[clap(short, long)]
     value: Option<U256>,
+    /// The TX nonce (pulled from RPC if omitted)
     #[clap(long)]
     nonce: Option<U256>,
+    /// The TX gas price (pulled from RPC if omitted)
     #[clap(long)]
     gas_price: Option<U256>,
+    /// The TX gas limit (estimated from RPC if omitted)
     #[clap(long)]
     gas: Option<U256>,
+    /// The tx data body (omit for simple sends)
     #[clap(short, long)]
     data: Option<String>,
+    /// The recipient/contract address
     #[clap(short, long)]
     to: Address,
+    /// The chain_id. see https://chainlist.org
+    #[clap(short, long)]
+    chain_id: Option<u64>,
 
     // AWS
+    /// AWS Key ID
     #[clap(short, long)]
     key_id: String,
-    #[clap(short, long)]
+    /// AWS Region string
+    #[clap(long)]
     region: String,
 
     // RPC
-    #[clap(short, long)]
+    /// RPC connection details
+    #[clap(long)]
     rpc: String,
 
     // Behavior
+    /// Print the tx req and signature instead of broadcasting
     #[clap(short, long)]
     print_only: bool,
 }
@@ -90,7 +103,10 @@ async fn _main() -> Result<()> {
 
     init_kms(opts.region.to_owned());
 
-    let signer = AwsSigner::new(KMS_CLIENT.get().unwrap(), opts.key_id.clone(), 0).await?;
+    let signer = AwsSigner::new(KMS_CLIENT.get().unwrap(), opts.key_id.clone(), 0)
+        .await?
+        .with_chain_id(opts.chain_id.unwrap_or(1));
+
     let provider = Provider::<Http>::try_from(opts.rpc.as_ref())?;
     let client = SignerMiddleware::new(provider, signer);
 
