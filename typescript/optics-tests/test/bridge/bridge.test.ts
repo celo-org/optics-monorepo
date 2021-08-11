ea044ef56c78088ce68992e7a7af135e87cf784e
anna-caroll <anna.s.carroll@gmail.com>
2021-08-11 14:52:48 +0200

    Refactor test, add comments, add event emitted check

--- typescript/optics-tests/test/bridge/bridge.test.ts
+++ typescript/optics-tests/test/bridge/bridge.test.ts
@@ -1,19 +1,23 @@
 import { ethers } from 'hardhat';
-
 import { Signer } from '../../lib/types';
 import { BigNumber, BytesLike } from 'ethers';
 import TestBridgeDeploy from '../../../optics-deploy/src/bridge/TestBridgeDeploy';
 import { toBytes32 } from '../../lib/utils';
 import { expect } from 'chai';
-import { IERC20, IERC20__factory } from '../../../typechain/optics-xapps';
+import {IERC20__factory, BridgeRouter, IERC20} from '../../../typechain/optics-xapps';
+
+async function getRepresentationTokenContract(deployer: Signer, bridgeRouter: BridgeRouter, domain: number, canonicalTokenAddress: BytesLike): Promise<IERC20> {
+  const reprAddr = await bridgeRouter['getLocalAddress(uint32,bytes32)'](domain, canonicalTokenAddress);
+  return IERC20__factory.connect(reprAddr, deployer);
+}
 
 describe.only('Bridge', async () => {
   let deployer: Signer;
   let deployerAddress: String;
   let deployerId: BytesLike;
-
   let deploy: TestBridgeDeploy;
   let transferAction: BytesLike;
+  let transferMessage: BytesLike;
 
   const DOMAIN = 1;
 
@@ -30,30 +34,28 @@ describe.only('Bridge', async () => {
   const TOKEN_VALUE = `0x${'00'.repeat(30)}ffff`;
 
   before(async () => {
+    // populate deployer signer
     [deployer] = await ethers.getSigners();
     deployerAddress = await deployer.getAddress();
-
-    deploy = await TestBridgeDeploy.deploy(deployer);
     deployerId = toBytes32(await deployer.getAddress());
+    // run test deploy of bridge contracts
+    deploy = await TestBridgeDeploy.deploy(deployer);
+    // generate transfer action
     transferAction = ethers.utils.concat([deployerId, TOKEN_VALUE]);
+    transferMessage = ethers.utils.concat([TOKEN_ID, transferAction]);
   });
 
   it('handles a transfer message', async () => {
-    const message = ethers.utils.concat([TOKEN_ID, transferAction]);
+    // first handle message for a new canonical token should deploy a representation token contract
+    expect(await deploy.contracts.bridgeRouter!.proxy.handle(
+        DOMAIN,
+        deployerId,
+        transferMessage,
+    )).to.emit(deploy.contracts.bridgeRouter!.proxy, "TokenDeployed");
 
-    await deploy.contracts.bridgeRouter!.proxy.handle(
-      DOMAIN,
-      deployerId,
-      message,
-    );
+    const repr: IERC20 = await getRepresentationTokenContract(deployer, deploy.contracts.bridgeRouter!.proxy, DOMAIN, CANONICAL_TOKEN_ADDRESS);
 
-    const reprAddr = await deploy.contracts.bridgeRouter!.proxy[
-      'getLocalAddress(uint32,bytes32)'
-    ](DOMAIN, CANONICAL_TOKEN_ADDRESS);
-    const repr = IERC20__factory.connect(reprAddr, deployer);
-
-    expect(await repr.balanceOf(deployer.address)).to.equal(
-      BigNumber.from(TOKEN_VALUE),
-    );
+    expect(await repr.balanceOf(deployer.address)).to.equal(BigNumber.from(TOKEN_VALUE));
+    expect(await repr.totalSupply()).to.equal(BigNumber.from(TOKEN_VALUE));
   });
 });
