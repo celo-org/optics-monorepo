cb142dc998841b053c591147a1498a49501a58c2
ember arlynx <ember@lunar.town>
2021-08-17 16:48:32 -0400

    rust agents: use OTLP for tracing
    the processor has also been tweaked
    
    impl Encode/Decode for u32

--- rust/optics-base/src/replica.rs
+++ rust/optics-base/src/replica.rs
@@ -10,6 +10,7 @@ use optics_core::{
 
 use optics_ethereum::EthereumReplica;
 use optics_test::mocks::MockReplicaContract;
+use tracing::instrument;
 
 /// Replica type
 #[derive(Debug)]
@@ -215,6 +216,8 @@ impl Common for Replicas {
         }
     }
 
+    // (i cannot remember the macro syntax)
+    #[instrument(fields(update = %update.update))]
     async fn update(&self, update: &SignedUpdate) -> Result<TxOutcome, ChainCommunicationError> {
         match self {
             Replicas::Ethereum(replica) => replica.update(update).await,
