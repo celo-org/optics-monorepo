059dbcdfc4de754240f9a6c81c03621214bfef9f
anna-caroll <anna.s.carroll@gmail.com>
2021-08-31 13:23:46 +0200

    Query enqueue on Home vs. Process on Replica

--- typescript/optics-deploy/scripts/query.ts
+++ typescript/optics-deploy/scripts/query.ts
@@ -12,11 +12,16 @@ import {EventFilter} from "@ethersproject/contracts/src.ts/index";
 
 const homeChain = kovan.chain;
 const remoteChain = alfajores.chain;
-const deployedAddresses = getDeployedAddresses(homeChain.config.name);
-const replicaAddress = deployedAddresses.replicas![remoteChain.config.domain].proxy;
+const homeAddresses = getDeployedAddresses(homeChain.config.name);
+const remoteAddresses = getDeployedAddresses(remoteChain.config.name);
+
+const replicaAddress = homeAddresses.replicas![remoteChain.config.domain].proxy;
 const replica: contracts.Replica = contracts.Replica__factory.connect(replicaAddress, homeChain.provider);
 
-getProcessEvents(replica).then(() => {
+const homeAddress = remoteAddresses.home.proxy;
+const alfaHome: contracts.Home = contracts.Home__factory.connect(homeAddress, remoteChain.provider);
+
+getProcessEvents(replica, alfaHome).then(() => {
     console.log("Done!");
     process.exit();
 });
@@ -49,10 +54,13 @@ async function getParsedEvents(contract: Contract, filter: EventFilter) {
     return parsedLogs;
 }
 
-async function getProcessEvents(replica: contracts.Replica) {
-    const processErrors = await getParsedEvents(replica, replica.filters.ProcessError());
+async function getProcessEvents(replica: contracts.Replica, home: contracts.Home) {
+    const dispatches = await getParsedEvents(home, home.filters.Dispatch());
+    console.log("Num dispatch on Alfa: ", dispatches.length);
+
     const processSuccesses = await getParsedEvents(replica, replica.filters.ProcessSuccess());
-    console.log(JSON.stringify(processErrors, null, 2));
-    console.log("Num process successes: ", processSuccesses.length);
+    console.log("Num process successes on Kovan: ", processSuccesses.length);
+
+    console.log("example dispatch: ", JSON.stringify(dispatches.slice(0, 2), null, 2));
 }
 
