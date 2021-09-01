af51ccb9c692d34363fc9338ab3bf15c9ea2c5dd
Erin Hales <mcbridee093@gmail.com>
2021-09-01 12:55:20 -0600

    test: two chain deploy

--- typescript/optics-tests/test/deploy.test.ts
+++ typescript/optics-tests/test/deploy.test.ts
@@ -6,7 +6,7 @@ import { getTestDeploy } from './testChain';
 import { Updater } from '../lib/core';
 import { Signer } from '../lib/types';
 import { CoreDeploy as Deploy } from '../../optics-deploy/src/core/CoreDeploy';
-import { deployNChains } from '../../optics-deploy/src/core';
+import { deployTwoChains, deployNChains } from '../../optics-deploy/src/core';
 import { BeaconProxy } from '../../optics-deploy/src/proxyUtils';
 
 const domains = [1000, 2000, 3000];
@@ -19,33 +19,41 @@ function assertBeaconProxy(beaconProxy: BeaconProxy<types.Contract>) {
 
 export async function deployCorrect(
   deploy: Deploy,
-  replicaDomains: number[],
+  remoteDomains: number[],
   governorDomain: number
 ) {
-  // console.log(deploy);
-
+  // Home upgrade setup contracts are defined
   assertBeaconProxy(deploy.contracts.home!);
+
+  // updaterManager is set on Home
   const updaterManager = await deploy.contracts.home?.proxy.updaterManager();
   expect(updaterManager).to.equal(deploy.contracts.updaterManager?.address);
 
+  // GovernanceRouter upgrade setup contracts are defined
   assertBeaconProxy(deploy.contracts.governance!);
 
-  replicaDomains.forEach(async domain => {
+  remoteDomains.forEach(async domain => {
+    // Replica upgrade setup contracts are defined
     assertBeaconProxy(deploy.contracts.replicas[domain]!);
+    // governanceRouter for remote domain is registered
     const registeredRouter = await deploy.contracts.governance?.proxy.routers(domain);
     expect(registeredRouter).to.not.equal(ethers.constants.AddressZero);
+    // replica is enrolled in xAppConnectionManager
     const enrolledReplica = await deploy.contracts.xAppConnectionManager?.domainToReplica(domain);
     expect(enrolledReplica).to.not.equal(ethers.constants.AddressZero);
+    //watchers have permission in xAppConnectionManager
     deploy.config.watchers.forEach(async watcher => {
       const watcherPermissions = await deploy.contracts.xAppConnectionManager?.watcherPermission(watcher, domain);
       expect(watcherPermissions).to.be.true;
     });
   });
 
+  // contracts are defined
   expect(deploy.contracts.updaterManager).to.not.be.undefined;
   expect(deploy.contracts.upgradeBeaconController).to.not.be.undefined;
   expect(deploy.contracts.xAppConnectionManager).to.not.be.undefined;
 
+  // governor is set on governor chain, empty on others
   const gov = await deploy.contracts.governance?.proxy.governor();
   const localDomain = await deploy.contracts.home?.proxy.localDomain()
   if (governorDomain == localDomain) {
@@ -53,12 +61,15 @@ export async function deployCorrect(
   } else {
     expect(gov).to.equal(ethers.constants.AddressZero);
   }
+  // governor domain is correct
   expect(await deploy.contracts.governance?.proxy.governorDomain()).to.equal(governorDomain);
 
+  // Home is set on xAppConnectionManager
   const xAppManagerHome = await deploy.contracts.xAppConnectionManager?.home();
   const homeAddress = deploy.contracts.home?.proxy.address
   expect(xAppManagerHome).to.equal(homeAddress);
 
+  // governor has ownership over following contracts
   const updaterManagerOwner = await deploy.contracts.updaterManager?.owner();
   const xAppManagerOwner = await deploy.contracts.xAppConnectionManager?.owner();
   const beaconOwner = await deploy.contracts.upgradeBeaconController?.owner();
@@ -73,9 +84,7 @@ export async function deployCorrect(
 /*
  * Deploy the full Optics suite on two chains
  */
-describe('DeployNChains', async () => {
-  let deploys: Deploy[] = [];
-
+describe.only('DeployNChains', async () => {
   let signer: Signer,
     recoveryManager: Signer,
     updater: Updater;
@@ -83,21 +92,30 @@ describe('DeployNChains', async () => {
   before(async () => {
     [signer, recoveryManager] = await ethers.getSigners();
     updater = await Updater.fromSigner(signer, domains[0]);
-
-    // get fresh test deploy objects
-    domains.forEach(async domain => {
-      deploys.push(await getTestDeploy(domain, updater.address, [recoveryManager.address]))
-    })
   });
 
-  beforeEach(async () => {
+  it('asserts all elements in three-chain deploy are correct', async () => {
+    let deploys: Deploy[] = [];
+    for (var i = 0; i < 3; i++) {
+      deploys.push(await getTestDeploy(domains[i], updater.address, [recoveryManager.address]));
+    }
     // deploy the entire Optics suite on each chain
     await deployNChains(deploys);
-  });
 
-  it('asserts all elements in deploy are defined', async () => {
     await deployCorrect(deploys[0], [domains[1], domains[2]], domains[0]);
     await deployCorrect(deploys[1], [domains[0], domains[2]], domains[0]);
     await deployCorrect(deploys[2], [domains[0], domains[1]], domains[0]);
+  });
+
+  it('asserts all elements in two-chain deploy are correct', async () => {
+    let deploys: Deploy[] = [];
+    for (var i = 0; i < 2; i++) {
+      deploys.push(await getTestDeploy(domains[i], updater.address, [recoveryManager.address]));
+    }
+
+    await deployTwoChains(deploys[0], deploys[1]);
+
+    await deployCorrect(deploys[0], [domains[1]], domains[0]);
+    await deployCorrect(deploys[1], [domains[0]], domains[0]);
   })
 });
