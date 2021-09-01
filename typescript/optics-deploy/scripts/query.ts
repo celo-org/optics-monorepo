ec34dbf3f06effa03f260388f1b9845d06faeb8b
anna-caroll <anna.s.carroll@gmail.com>
2021-09-01 12:45:55 +0200

    Add code to check status of dispatches within transaction hash

--- typescript/optics-deploy/scripts/query.ts
+++ typescript/optics-deploy/scripts/query.ts
@@ -10,14 +10,40 @@ import {
 } from "../src/verification/readDeployOutput";
 import {Chain, CoreContractDeployOutput} from "../src/chain";
 import {EventFilter} from "@ethersproject/contracts/src.ts/index";
+import * as ethers from "ethers";
 
-checkEvents(kovan.chain, alfajores.chain).then(() => {
-    console.log("");
-    checkEvents(alfajores.chain, kovan.chain).then(() => {
-        console.log("Done!");
-        process.exit();
-    });
+checkStatus(alfajores.chain, kovan.chain, "0x0c648f10649d65139a1748b0005a59ac04bee44348dbeac3671a17a9aac2034c").then(() => {
+    console.log("Done!");
+    process.exit();
 });
+
+async function checkStatus(homeChain: Chain, replicaChain: Chain, transactionHash: string) {
+    try {
+    const home = getHome(homeChain);
+    const replica = getReplicaForHome(homeChain, replicaChain);
+
+    const dispatches = await getDispatches(transactionHash, homeChain, home);
+    for (let dispatch of dispatches) {
+        const committedRoot = dispatch["args"][3];
+        const messageHash = dispatch["args"][2];
+        console.log("Message Hash: ", messageHash);
+
+        const homeUpdate = await getHomeUpdate(committedRoot, home);
+        if (!homeUpdate) {
+            continue;
+        }
+
+        const replicaUpdate = await getReplicaUpdate(committedRoot, replica);
+        if (!replicaUpdate) {
+            continue;
+        }
+
+        await getReplicaProcessResult(messageHash, replica);
+    }
+    } catch (e) {
+        console.error(e);
+    }
+}
 
 async function checkEvents(homeChain: Chain, replicaChain: Chain) {
     const home = getHome(homeChain);
@@ -40,6 +66,54 @@ async function checkEvents(homeChain: Chain, replicaChain: Chain) {
     console.log(`Backlog ${homeName} => ${replicaName}: `, unprocessedDispatches.length);
 
     writeQueryResults(homeName, replicaName, dispatchedLeaves, processSuccessLeaves, processErrors, unprocessedDispatches);
+}
+
+async function getDispatches(transactionHash: string, homeChain: Chain, home: contracts.Home) {
+    return getLogsFromTransaction(transactionHash, homeChain.provider, home, home.filters.Dispatch());
+}
+
+async function getHomeUpdate(committedRoot: string, home: contracts.Home) {
+    const updates = await getLogs(home, home.filters.Update(null, committedRoot));
+    const success = updates.length > 0;
+    console.log(`Home Update: ${success ? "SUCCESS" : "PENDING"}`);
+    if (success) {
+        const update = updates[0];
+        console.log(`   Hash: ${update["transactionHash"]}`);
+        return update;
+    }
+}
+
+async function getReplicaUpdate(committedRoot: string, replica: contracts.Replica) {
+    const updates = await getLogs(replica, replica.filters.Update(null, committedRoot));
+    const success = updates.length > 0;
+    console.log(`Replica Update: ${success ? "SUCCESS" : "PENDING"}`);
+    if (success) {
+        const update = updates[0];
+        console.log(`   Hash: ${update["transactionHash"]}`);
+        return update;
+    }
+}
+
+async function getReplicaProcessResult(messageHash: string, replica: contracts.Replica) {
+    const processSuccesses = await getLogs(replica, replica.filters.ProcessSuccess(messageHash));
+    const success = processSuccesses.length > 0;
+    if (success) {
+        console.log(`Replica Process SUCCESS`);
+        const success = processSuccesses[0];
+        console.log(`   Hash: ${success["transactionHash"]}`);
+        return success;
+    } else {
+        const processFailures = await getLogs(replica, replica.filters.ProcessError(messageHash));
+        const failure = processFailures.length > 0;
+        if(failure) {
+            console.log(`Replica Process FAILED`);
+            const fail = processFailures[0];
+            console.log(`   Hash: ${fail["transactionHash"]}`);
+            return fail;
+        } else {
+            console.log(`Replica Process PENDING`);
+        }
+    }
 }
 
 function writeQueryResults(homeName: string, replicaName: string, dispatchedLeaves: any[], processSuccessLeaves: any[], processErrors: any[], unprocessedDispatches: any[]) {
@@ -102,6 +176,22 @@ function getDeployedAddresses(name: string) {
 async function getLogs(contract: Contract, filter: EventFilter) {
     const logs = await contract.queryFilter(filter);
     const fullLogs = logs.map((log) => {
+        const parsedLog = contract.interface.parseLog(log);
+        return {
+            ...log,
+            ...parsedLog,
+        };
+    });
+    return fullLogs;
+}
+
+async function getLogsFromTransaction(transactionHash: string, provider: ethers.providers.Provider, contract: Contract, filter: EventFilter) {
+    const transactionReceipt = await provider.getTransactionReceipt(transactionHash);
+    if (!transactionReceipt) {
+        return [];
+    }
+    const targetLogs = transactionReceipt.logs.filter((log) => log.address == contract.address)
+    const fullLogs = targetLogs.map((log) => {
         const parsedLog = contract.interface.parseLog(log);
         return {
             ...log,
