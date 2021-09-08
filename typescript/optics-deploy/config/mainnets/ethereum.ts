10cedb2ea262c5e2fe36af3b4a934ac3f4fd5130
James Prestwich <james@prestwi.ch>
2021-09-08 12:56:38 -0700

    chore: update mainnet configs with msig governor

--- typescript/optics-deploy/config/mainnets/ethereum.ts
+++ typescript/optics-deploy/config/mainnets/ethereum.ts
@@ -22,14 +22,16 @@ export const chain = toChain(chainJson);
 export const config: CoreConfig = {
   environment: 'prod',
   updater: '0xDB2091535eb0Ee447Ce170DDC25204FEA822dd81',
-  watchers: ['0xeE42B7757798cf495CDaA8eDb0CC237F07c60C81'],
+  recoveryTimelock: 60 * 60 * 24, // 1 day
   recoveryManager: '0x3D9330014952Bf0A3863FEB7a657bfFA5C9D40B9',
+  optimisticSeconds: 60 * 60 * 3, // 3 hours
+  watchers: ['0xeE42B7757798cf495CDaA8eDb0CC237F07c60C81'],
   governor: {
     domain: chainJson.domain,
-    address: '0x04c0Ba808bF45476A6A031aa86b69e7FbAe8c56C',
+    address: '0x5Fa96B622D1F4e920b92040c10fA297ca496ad37',
   },
-  optimisticSeconds: 60 * 60 * 3, // 3 hours
-  recoveryTimelock: 60 * 60 * 24, // 1 day
+  processGas: 850_000,
+  reserveGas: 15_000,
 };
 
 export const bridgeConfig: BridgeConfig = {
