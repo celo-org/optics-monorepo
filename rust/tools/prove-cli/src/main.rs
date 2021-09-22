07008e184fe894480b910c921b478cfec1393290
James Prestwich <james@prestwi.ch>
2021-09-22 15:32:53 -0400

    refactor: improve prove cli legibility

--- rust/tools/prove-cli/src/main.rs
+++ rust/tools/prove-cli/src/main.rs
@@ -1,6 +1,7 @@
 use std::{convert::TryFrom, sync::Arc};
 
 use optics_core::{
+    accumulator::merkle::Proof,
     db::DB,
     traits::{MessageStatus, Replica},
     Decode, OpticsMessage,
@@ -11,16 +12,23 @@ use clap::Clap;
 use ethers::{
     core::k256::ecdsa::SigningKey,
     prelude::{Http, Middleware, Provider, SignerMiddleware},
+    types::H256,
 };
 
-use color_eyre::Result;
+use color_eyre::{eyre::bail, Result};
 use ethers_signers::{Signer, Wallet};
+
+type ConcreteReplica = EthereumReplica<SignerMiddleware<Provider<Http>, Wallet<SigningKey>>>;
 
 #[derive(Clap)]
 struct Opts {
     /// Leaf index to prove
     #[clap(short, long)]
-    leaf_index: u32,
+    leaf_index: Option<u32>,
+
+    /// Leaf index to prove
+    #[clap(short, long)]
+    leaf_hash: Option<H256>,
 
     /// Path to db containing proof
     #[clap(long)]
@@ -39,26 +47,51 @@ struct Opts {
     rpc: String,
 }
 
+impl Opts {
+    fn fetch_proof(&self) -> Result<(OpticsMessage, Proof)> {
+        let db = DB::from_path(&self.db)?;
+
+        let idx = if let Some(idx) = self.leaf_index {
+            idx
+        } else if let Some(leaf_hash) = self.leaf_hash {
+            match db.message_by_leaf_hash(leaf_hash)? {
+                Some(leaf) => leaf.leaf_index,
+                None => bail!("No leaf index or "),
+            }
+        } else {
+            bail!("Must provide leaf index or leaf hash");
+        };
+
+        let proof = db.proof_by_leaf_index(idx)?.expect("no proof");
+        let message = db.message_by_leaf_index(idx)?.expect("no message");
+        let message = OpticsMessage::read_from(&mut message.message.as_slice())?;
+
+        Ok((message, proof))
+    }
+
+    async fn replica(&self) -> Result<ConcreteReplica> {
+        let provider = Provider::<Http>::try_from(self.rpc.as_ref())?;
+        let signer = self
+            .key
+            .parse::<Wallet<SigningKey>>()?
+            .with_chain_id(provider.get_chainid().await?.low_u64());
+
+        let middleware = SignerMiddleware::new(provider, signer);
+        Ok(EthereumReplica::new(
+            "",
+            0,
+            self.address.parse()?,
+            Arc::new(middleware),
+        ))
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     let opts = Opts::parse();
-    let provider = Provider::<Http>::try_from(opts.rpc.as_ref())?;
-    let signer = opts
-        .key
-        .parse::<Wallet<SigningKey>>()?
-        .with_chain_id(provider.get_chainid().await?.low_u64());
 
-    let middleware = SignerMiddleware::new(provider, signer);
-
-    let replica = EthereumReplica::new("", 0, opts.address.parse()?, Arc::new(middleware));
-
-    let db = DB::from_path(&opts.db)?;
-
-    let proof = db.proof_by_leaf_index(opts.leaf_index)?.expect("no proof");
-    let message = db
-        .message_by_leaf_index(opts.leaf_index)?
-        .expect("no message");
-    let message = OpticsMessage::read_from(&mut message.message.clone().as_slice())?;
+    let (message, proof) = opts.fetch_proof()?;
+    let replica = opts.replica().await?;
 
     let status = replica.message_status(message.to_leaf()).await?;
     let outcome = match status {
