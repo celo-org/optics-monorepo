387793d4a6a527eb095f40ee73e8f573e96667ce
anna-caroll <anna.s.carroll@gmail.com>
2021-09-28 01:11:56 +0200

    feat: events function

--- typescript/optics-provider/src/optics/messages/OpticsMessage.ts
+++ typescript/optics-provider/src/optics/messages/OpticsMessage.ts
@@ -32,17 +32,6 @@ export function parseMessage(message: string): ParsedMessage {
   return { from, sender, nonce, destination, recipient, body };
 }
 
-function tryDispatchEvent(log: Log): DispatchEvent | undefined {
-  const home = new core.Home__factory().interface;
-  try {
-    const parsed = home.parseLog(log);
-    if (parsed.name === 'Dispatch') {
-      return parsed as unknown as DispatchEvent;
-    }
-  } catch (e) {}
-  return undefined;
-}
-
 export class OpticsMessage {
   readonly event: DispatchEvent;
   readonly receipt: TransactionReceipt;
@@ -51,7 +40,10 @@ export class OpticsMessage {
   readonly destinationAndNonce: BigNumber;
   readonly committedRoot: string;
   readonly message: ParsedMessage;
+  readonly home: core.Home;
+  readonly replica: core.Replica;
   protected context: OpticsContext;
+  protected eventProvider: MultiEvents;
 
   constructor(event: DispatchEvent, receipt: TransactionReceipt, context: OpticsContext) {
     this.event = event;
@@ -62,6 +54,9 @@ export class OpticsMessage {
     this.committedRoot = event.args.committedRoot;
     this.message = parseMessage(event.args.message);
     this.context = context;
+    this.eventProvider = new MultiEvents(context);
+    this.home = context.mustGetCore(this.message.from).home;
+    this.replica = context.mustGetReplicaFor(this.message.from, this.message.destination);
   }
 
   static async fromTransactionHash(
@@ -69,7 +64,8 @@ export class OpticsMessage {
       transactionHash: string,
       context: OpticsContext,
   ): Promise<OpticsMessage[]> {
-    const receipt = await context.getTransactionReceipt(nameOrDomain, transactionHash);
+    const provider = context.mustGetProvider(nameOrDomain);
+    const receipt = await provider.getTransactionReceipt(transactionHash);
     return OpticsMessage.fromReceipt(receipt!, context);
   }
 
@@ -78,7 +74,8 @@ export class OpticsMessage {
       transactionHash: string,
       context: OpticsContext,
   ): Promise<OpticsMessage> {
-    const receipt = await context.getTransactionReceipt(nameOrDomain, transactionHash);
+    const provider = context.mustGetProvider(nameOrDomain);
+    const receipt = await provider.getTransactionReceipt(transactionHash);
     return OpticsMessage.singleFromReceipt(receipt!, context);
   }
 
@@ -88,7 +85,7 @@ export class OpticsMessage {
   ): OpticsMessage[] {
     let messages: OpticsMessage[] = [];
     for (const log of receipt.logs) {
-      const event = tryDispatchEvent(log);
+      const event = MultiEvents.tryDispatchEvent(log);
       if (event) {
         const message = new OpticsMessage(event, receipt, context);
         messages.push(message);
@@ -108,20 +105,76 @@ export class OpticsMessage {
     return messages[0];
   }
 
-  async status(): Promise<MessageStatus> {
-    const replica = this.context.getReplicaFor(this.from, this.destination);
-    if (!replica) {
-      throw new Error(
-        `No replica on ${this.destination} for home ${this.from}`,
-      );
-    }
+  dispatchEvent(): DispatchEvent {
+    return this.event;
+  }
 
-    return await replica.messages(this.messageHash);
+  async homeUpdateEvent(): Promise<UpdateEvent> {
+    const updateFilter = this.home.filters.Update(this.from, this.committedRoot);
+    const updateLogs = await this.eventProvider.getEvents(this.from, this.home, updateFilter);
+    return updateLogs[0] as unkown as UpdateEvent;
+  }
+
+  async replicaUpdateEvent(): Promise<UpdateEvent> {
+    const updateFilter = this.replica.filters.Update(this.from, this.committedRoot);
+    const updateLogs = await this.eventProvider.getEvents(this.destination, this.replica, updateFilter);
+    return updateLogs[0] as unkown as UpdateEvent;
+  }
+
+  async processEvent(): Promise<ProcessEvent> {
+    const processFilter = this.replica.filters.Process(this.messageHash);
+    const processLogs = await this.eventProvider.getEvents(this.destination, this.replica, processFilter);
+    return processLogs[0] as unkown as ProcessEvent;
+  }
+
+  async events(): Promise<OpticsStatus> {
+    const events: OpticsEvent[] = [this.event];
+    // attempt to get Home update
+    const homeUpdate = await this.homeUpdateEvent();
+    if (!homeUpdate) {
+      return {
+        status: "Dispatched", // the message has been sent; nothing more
+        events
+      };
+    }
+    events.push(homeUpdate);
+    // attempt to get Replica update
+    const replicaUpdate = await this.replicaUpdateEvent();
+    if (!replicaUpdate) {
+      return {
+        status: "Included", // the message was sent, then included in an Update on Home
+        events
+      };
+    }
+    events.push(replicaUpdate);
+    // attempt to get Replica process
+    const process = await this.processEvent();
+    if (!process) {
+      return {
+        status: "Relayed", // the message was sent, included in an Update, then relayed to the Replica
+        events
+      };
+    }
+    events.push(process);
+    return {
+      status: "Processed", // the message was processed
+      events
+    };
+  }
+
+  async confirmAt(): Promise<BigNumber> {
+    const update = await this.replicaUpdateEvent();
+    const {newRoot} = update.args;
+    return this.replica.confirmAt(newRoot);
+  }
+
+  async replicaStatus(): Promise<MessageStatus> {
+    return this.replica.messages(this.messageHash);
   }
 
   /// Returns true when the message is delivered
   async delivered(): Promise<boolean> {
-    const status = await this.status();
+    const status = await this.replicaStatus();
     return status === MessageStatus.Processed;
   }
 
