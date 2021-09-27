06e38ecddbcfc55708baa8b57e0622b8e150b062
Erin Hales <mcbridee093@gmail.com>
2021-09-27 10:07:33 -0600

    export types

--- typescript/optics-provider/src/optics/tokens/testnetWellKnown.ts
+++ typescript/optics-provider/src/optics/tokens/testnetWellKnown.ts
@@ -1,12 +1,12 @@
 import { TokenIdentifier } from './index';
 
 const Alfajores: TokenIdentifier = {
-  domain: 'celo',
+  domain: 'alfajores',
   id: '0xF194afDf50B03e69Bd7D057c1Aa9e10c9954E4C9',
 };
 
 const Kovan: TokenIdentifier = {
-  domain: 'ethereum',
+  domain: 'kovan',
   id: '0xd0a1e359811322d97991e03f863a0c30c2cf029c',
 };
 
