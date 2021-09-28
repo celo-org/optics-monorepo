a81aeb7cec1b3e79321b057ac01b4fc2cc1564e5
anna-caroll <anna.s.carroll@gmail.com>
2021-09-28 20:33:57 +0200

    Refactor: tracing messages

--- tools/contract-metrics/messageTracer.ts
+++ tools/contract-metrics/messageTracer.ts
@@ -1,13 +1,16 @@
-import { mainnet, OpticsContext } from "@optics-xyz/multi-provider";
-import { core } from '@optics-xyz/ts-interface';
 import config from "./config";
-import {getLogs} from "./events"
+import {mainnet, OpticsContext, OpticsMessage, OpticsStatus, MessageStatus, OpticsEvent} from "../../typescript/optics-provider/src/optics";
 
 mainnet.registerRpcProvider('celo', config.CeloRpc);
 mainnet.registerRpcProvider('ethereum', config.EthereumRpc);
 mainnet.registerRpcProvider('polygon', config.PolygonRpc);
 
-const networks = [{name: "ethereum", blockHeight: 13187674}, {name: "celo", blockHeight: 8712249}, {name: "polygon", blockHeight: 18895794}]
+const STATUS_TO_STRING = {
+    [MessageStatus.Dispatched]: "Dispatched on Home",
+    [MessageStatus.Included]: "Included in Home Update",
+    [MessageStatus.Relayed]: "Relayed to Replica",
+    [MessageStatus.Processed]: "Processed"
+};
 
 const input: TraceInput[] = [
     {
@@ -36,152 +39,46 @@ interface TraceInput {
     leafIndex?: number,
 }
 
-interface TraceDetail {
-    chain: string,
-    domain: number,
-    event: string, 
-    blockNumber: number, 
-    transactionHash: string,
-    metadata: any
+interface TraceOutput {
+    status: string,
+    events: QuietEvent[]
 }
 
-async function getUpdateDetails(context: OpticsContext, home: core.Home | core.Replica, network: string, domain: number, committedRoot: string): Promise<TraceDetail> {
-    const updateLogFilter = home.filters.Update(undefined, committedRoot);
-    const homeUpdateLogs = await getLogs(network, context, home, updateLogFilter);
-    const updateLog = homeUpdateLogs[0];
+interface QuietEvent {
+    event: string,
+    nameOrDomain: string | number;
+    blockNumber: number;
+    transactionHash: string;
+}
 
-    try {
-        const updateTx = await updateLog.getTransaction();
-        return {
-            chain: network,
-            domain: domain,
-            event: "Update",
-            blockNumber: updateTx.blockNumber!,
-            transactionHash: updateTx.hash,
-            metadata: {
-                homeDomain: updateLog!.args!.homeDomain,
-                newRoot: committedRoot,
-                oldRoot: updateLog!.args!.oldRoot
-            }
-        };
-    } catch {
-        console.log("Problem getting Update event...");
-        return {
-            chain: network,
-            domain: domain,
-            event: "Update",
-            blockNumber: 0,
-            transactionHash: "N/A",
-            metadata: {
-                error: "Transaction Not Found"
-            }
-        };
+function transformEvent(opticsEvent: OpticsEvent) {
+    // TODO: transform nameOrDomain to human readable
+    // TODO: add link to block explorer????
+    const {event, nameOrDomain, blockNumber, transactionHash} = opticsEvent;
+    return {
+        event: event!,
+        nameOrDomain,
+        blockNumber,
+        transactionHash,
     }
 }
 
-async function getProcessDetails(context: OpticsContext, replica: core.Replica, network: string, domain: number, messageHash: string): Promise<TraceDetail> {
-    const processFilter = replica.filters.Process(messageHash);
-    const processLogs = await getLogs(network, context, replica, processFilter);
-    const processLog = processLogs[0];
-
-    try {
-        const processTx = await processLog.getTransaction();
-        return {
-            chain: network,
-            domain: domain,
-            event: "ProcessReplica",
-            blockNumber: processTx.blockNumber!,
-            transactionHash: processTx.hash,
-            metadata: {}
-        };
-    } catch {
-        console.log("Problem getting process event...")
-        return {
-            chain: network,
-            domain: domain,
-            event: "ProcessReplica",
-            blockNumber: 0,
-            transactionHash: "N/A",
-            metadata: {
-                error: "Transaction Not Found"
-            }
-        };
-    }
-}
-
-
-async function getDispatchDetails(context: OpticsContext, home: core.Home, network: string, domain: number, transactionHash: string): Promise<TraceDetail | undefined> {
-    const provider = context.getProvider(network)!;
-
-    let transaction;
-    try {
-        transaction = await provider.getTransaction(transactionHash);
-    } catch (error) {
-        console.log(error);
-        console.log(`The Send Transaction ID Doesn't exist on ${network}... Are you sure you got it right?`);
-        return;
-    };
-
-    // TODO: get destination domain from destinationAndSequence
-    // on Dispatch event using OpticsMessage class (skip querying for Send event)
-    const bridgeRouter = context.mustGetBridge(network).bridgeRouter;
-    const sendFilter = bridgeRouter.filters.Send();
-    const sendLogs = await getLogs(network, context, bridgeRouter, sendFilter, transaction.blockNumber!);
-    const {toDomain} = sendLogs[0]!.args!;
-    const destinationDomain = toDomain;
-
-    const dispatchFilter = home.filters.Dispatch();
-    const dispatchLogs = await getLogs(network, context, home, dispatchFilter, transaction.blockNumber!);
-    const {committedRoot, messageHash, leafIndex} = dispatchLogs[0]!.args!;
+function transformStatus(opticsStatus: OpticsStatus): TraceOutput {
+    const {status, events} = opticsStatus;
 
     return {
-        chain: network,
-        domain: domain,
-        event: "Send",
-        blockNumber: transaction.blockNumber!,
-        transactionHash: transaction.hash,
-        metadata: {
-            destinationDomain,
-            committedRoot: committedRoot,
-            messageHash: messageHash,
-            leafIndex: leafIndex.toNumber()
-        }
-    };
+        status: STATUS_TO_STRING[status],
+        events: events.map(event => transformEvent(event))
+    }
 }
 
 async function traceTransfer(context: OpticsContext, origin:string, transactionHash:string) {
-    console.log(`Beginning Trace of ${transactionHash} on ${origin}`);
-    const details: TraceDetail[] = [];
+    console.log(`Trace ${transactionHash} on ${origin}`);
 
-    // get Home contract and network
-    const originHome = context.mustGetCore(origin).home;
-    const originDomain = context.resolveDomain(origin);
-    const originNetwork = (await originHome.provider.getNetwork()).name;
-
-    // Hop One - Send Transaction
-    const dispatch = await getDispatchDetails(context, originHome, origin, originDomain, transactionHash);
-    if(!dispatch) return;
-    details.push(dispatch);
-
-    // pull Dispatch details from metadata
-    const {destinationDomain, committedRoot, messageHash} = dispatch.metadata;
-
-    // Hop Two - The Update Transaction
-    const homeUpdate = await getUpdateDetails(context, originHome, originNetwork, originDomain, committedRoot);
-    details.push(homeUpdate);
-
-    // get Replica contract and network
-    const replica = context.mustGetCore(destinationDomain).replicas.get(originDomain)!.contract;
-    let replicaNetwork = (await replica.provider.getNetwork()).name;
-
-    // Hop Three - The Replica Update Transaction
-    const replicaUpdate = await getUpdateDetails(context, replica, replicaNetwork, destinationDomain, committedRoot);
-    details.push(replicaUpdate);
-
-    // Hop Four - Process Transaction
-    const replicaProcess = await getProcessDetails(context, replica, replicaNetwork, destinationDomain, messageHash);
-    details.push(replicaProcess);
+    const message = await OpticsMessage.singleFromTransactionHash(context, origin, transactionHash);
+    const status = await message.events();
+    const printableStatus = transformStatus(status);
 
     // Log Details
-    console.log(details)
+    console.log(JSON.stringify(printableStatus, null, 2));
 }
