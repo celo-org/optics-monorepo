bdf474fd5ce93a80f12d0a26b1777b63c9f43920
Luke Tchang <ltchang@stanford.edu>
2021-09-30 22:11:15 -0700

    fix: decomposes run function and tidies up printing

--- rust/tools/optics-cli/src/subcommands/db_state.rs
+++ rust/tools/optics-cli/src/subcommands/db_state.rs
@@ -20,13 +20,25 @@ pub struct DbStateCommand {
     home_name: String,
 }
 
+type OutputVec = Vec<((H256, u64), Vec<CommittedMessage>)>;
+
 impl DbStateCommand {
     #[allow(dead_code)]
     pub async fn run(&self) -> Result<()> {
-        println!("Run!");
         let db = HomeDB::new(DB::from_path(&self.db_path)?, self.home_name.clone());
 
-        println!("Creating mapping of committed roots to messages");
+        let messages_by_committed_roots = DbStateCommand::create_comitted_root_to_message_map(&db)?;
+
+        let output_vec = DbStateCommand::create_output_vec(&db, messages_by_committed_roots)?;
+
+        DbStateCommand::print_output(output_vec);
+
+        Ok(())
+    }
+
+    fn create_comitted_root_to_message_map(
+        db: &HomeDB,
+    ) -> Result<HashMap<H256, Vec<CommittedMessage>>> {
         let mut messages_by_committed_roots: HashMap<H256, Vec<CommittedMessage>> = HashMap::new();
         for index in 0.. {
             match db.message_by_leaf_index(index)? {
@@ -35,16 +47,16 @@ impl DbStateCommand {
                     let bucket_opt = messages_by_committed_roots.get_mut(&committed_root);
 
                     // Get reference to bucket for committed root
-                    let bucket;
-                    if bucket_opt.is_none() {
-                        messages_by_committed_roots
-                            .insert(committed_root, Vec::<CommittedMessage>::new());
-                        bucket = messages_by_committed_roots
-                            .get_mut(&committed_root)
-                            .unwrap();
-                    } else {
-                        bucket = bucket_opt.unwrap();
-                    }
+                    let bucket = match bucket_opt {
+                        Some(bucket) => bucket,
+                        None => {
+                            messages_by_committed_roots
+                                .insert(committed_root, Vec::<CommittedMessage>::new());
+                            messages_by_committed_roots
+                                .get_mut(&committed_root)
+                                .unwrap()
+                        }
+                    };
 
                     // Add message to bucket for committed root
                     bucket.push(message.try_into()?);
@@ -53,8 +65,14 @@ impl DbStateCommand {
             }
         }
 
-        // Create mapping of (update_root, block_number) --> [messages]
-        println!("Creating mapping of update roots to messages");
+        Ok(messages_by_committed_roots)
+    }
+
+    fn create_output_vec(
+        db: &HomeDB,
+        messages_by_committed_roots: HashMap<H256, Vec<CommittedMessage>>,
+    ) -> Result<OutputVec> {
+        // Create mapping of (update root, block_number) to [messages]
         let mut output_map: HashMap<(H256, u64), Vec<CommittedMessage>> = HashMap::new();
         for (committed_root, bucket) in messages_by_committed_roots {
             let containing_update_opt = db.update_by_previous_root(committed_root)?;
@@ -71,25 +89,31 @@ impl DbStateCommand {
                             )
                         });
 
-                        output_map.insert((new_root, update_block_number), bucket);
+                    output_map.insert((new_root, update_block_number), bucket);
                 }
                 // No more updates left
                 None => break,
             }
         }
 
-        for ((comitted_root, block_number), bucket) in output_map {
-            println!("Committed root: {}", comitted_root);
+        let mut output_vec: Vec<_> = output_map.into_iter().collect();
+        output_vec.sort_by(|x, y| x.0 .1.cmp(&y.0 .1));
+
+        Ok(output_vec)
+    }
+
+    fn print_output(output_vec: Vec<((H256, u64), Vec<CommittedMessage>)>) {
+        for ((comitted_root, block_number), mut bucket) in output_vec {
+            println!("Update root: {:?}", comitted_root);
             println!("Block number: {}", block_number);
 
-            println!("Leaves:");
+            bucket.sort_by(|x, y| x.leaf_index.cmp(&y.leaf_index));
+            print!("Leaves:");
             for message in bucket {
-                println!("\t{:?}", message);
+                print!(" {} ", message.leaf_index);
             }
-            
-            println!();
-        }
 
-        Ok(())
+            println!("\n");
+        }
     }
 }
