3d2fbdf9586cd8effe2a873e505be6c2ee1e57cc
ember arlynx <ember.arlynx@clabs.co>
2021-10-13 17:11:54 -0400

    doctest the whole chain

--- rust/chains/optics-ethereum/src/lib.rs
+++ rust/chains/optics-ethereum/src/lib.rs
@@ -1,5 +1,5 @@
 //! Interfaces to the ethereum contracts
-
+#![cfg(not(doctest))]
 #![forbid(unsafe_code)]
 // #![warn(missing_docs)]
 #![warn(unused_extern_crates)]
@@ -7,23 +7,18 @@
 mod macros;
 
 /// Home abi
-#[cfg(not(doctest))]
-mod home;
-
+pub(crate) mod home;
 mod home_indexer;
 
 /// Replica abi
-#[cfg(not(doctest))]
 mod replica;
 
 /// XAppConnectionManager abi
-#[cfg(not(doctest))]
 mod xapp;
 
 /// Configuration structs
 pub mod settings;
 
-#[cfg(not(doctest))]
 pub use crate::{
     home::EthereumHome, home_indexer::*, replica::EthereumReplica, xapp::EthereumConnectionManager,
 };
