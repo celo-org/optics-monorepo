dc79ca769c7f5bdf961331dc2875ab86d8ae7ce8
Luke Tchang <ltchang@stanford.edu>
2021-10-16 22:55:52 -0700

    fix: put watcher back into cargo.toml and fix OpticsDB there

--- rust/optics-core/src/db/mod.rs
+++ rust/optics-core/src/db/mod.rs
@@ -11,8 +11,8 @@ mod typed_db;
 pub use typed_db::*;
 
 /// DB operations tied to specific home
-mod agent_db;
-pub use agent_db::*;
+mod optics_db;
+pub use optics_db::*;
 
 use crate::{Decode, Encode, OpticsError};
 
